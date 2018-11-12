7749c898afc5b30bd97fc82135fca2c67d324bd9
Jhonny Moreira <jhonny.moreira@yld.io>
2018-11-12 12:23:32 +0000

    workstages without css

--- src/templates/service.js
+++ src/templates/service.js
@@ -4,11 +4,37 @@ import { graphql } from 'gatsby'
 import { Row, Col, Grid } from 'react-styled-flexboxgrid'
 import { H1 } from '../components/Typography'
 import Layout from '../components/layout'
+import CaseStudy from '../components/Homepage/caseStudy'
+
+const WorkStage = ({ workStage }) => {
+  const sections = Array(5)
+    .fill({})
+    .map((element, index) => ({
+      sectionTitle: workStage[`sectionTitle${index + 1}`],
+      ...(workStage[`sectionBody${index + 1}`] && {
+        sectionBody: workStage[`sectionBody${index + 1}`][`sectionBody${index + 1}`]
+      }),
+      ...(workStage[`sectionIcon${index + 1}`] && {
+        sectionIcon: workStage[`sectionIcon${index + 1}`]
+      })
+    }))
+    .filter(({ sectionTitle }) => sectionTitle)
+  return (
+    <div>
+      <H1>{workStage.title}</H1>
+      {sections.map(({ sectionTitle, sectionBody }) => (
+        <div key={sectionTitle}>
+          <p>{sectionTitle}</p>
+          <p>{sectionBody}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 const Service = ({ data }) => {
   const service = data.allContentfulService.edges[0].node
   const site = data.site
-
   return (
     <Layout>
       <Helmet
@@ -22,7 +48,14 @@ const Service = ({ data }) => {
       <Grid className="grid">
         <Row>
           <Col xs={12}>
-            <H1>{service.title}</H1>
+            <CaseStudy caseStudy={service.case_study[0]} />
+            <p>{service.mainPageIntroSentence.mainPageIntroSentence}</p>
+            <div>
+              <H1>{service.workStagesTitle}</H1>
+              {service.workStages.map(workStage => (
+                <WorkStage key={workStage.id} workStage={workStage} />
+              ))}
+            </div>
           </Col>
         </Row>
       </Grid>
@@ -46,6 +79,80 @@ export const pageQuery = graphql`
           title
           seoTitle
           seoMetaDescription
+          mainPageIntroSentence {
+            mainPageIntroSentence
+          }
+          case_study {
+            id
+            title
+            slug
+            posterImage {
+              file {
+                url
+              }
+            }
+            posterColor
+            body {
+              content {
+                content {
+                  value
+                  nodeType
+                }
+              }
+            }
+          }
+          workStagesTitle
+          workStages {
+            id
+            title
+            sectionTitle1
+            sectionTitle2
+            sectionTitle3
+            sectionTitle4
+            sectionTitle5
+            sectionIcon1 {
+              file {
+                url
+              }
+            }
+            sectionIcon2 {
+              file {
+                url
+              }
+            }
+            sectionIcon3 {
+              file {
+                url
+              }
+            }
+            sectionIcon4 {
+              file {
+                url
+              }
+            }
+            sectionIcon5 {
+              file {
+                url
+              }
+            }
+            sectionBody1 {
+              sectionBody1
+            }
+            sectionBody2 {
+              sectionBody2
+            }
+            sectionBody3 {
+              sectionBody3
+            }
+
+            sectionBody4 {
+              sectionBody4
+            }
+
+            sectionBody5 {
+              sectionBody5
+            }
+          }
         }
       }
     }
