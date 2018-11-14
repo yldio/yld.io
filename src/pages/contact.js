fa75b16093c0850ebbd5ec3a6bb950cd13380279
zooeymiller <zooeyxmiller@gmail.com>
2018-11-14 10:47:58 +0000

    reorder contact checkboxes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,12 +17,12 @@ import {
 import GrayBG from '../components/GrayBG'
 
 const checkboxes = [
-  { name: 'join', label: 'Join our team' },
   { name: 'training', label: 'Training services' },
   { name: 'engineering', label: 'Engineering services' },
   { name: 'design', label: 'Design services' },
   { name: 'sponsor', label: 'Sponsor an event' },
   { name: 'speak', label: 'Speak at an event' },
+  { name: 'join', label: 'Join our team' },
   { name: 'none', label: 'None of these' }
 ]
 
