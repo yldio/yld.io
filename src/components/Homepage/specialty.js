dd9c9d8020bd6da47ddeb35fbbf2d79c9480fde5
Jhonny Moreira <jhonny.moreira@yld.io>
2018-11-21 15:10:45 +0000

    related projects

--- src/components/Homepage/specialty.js
+++ src/components/Homepage/specialty.js
@@ -1,56 +1,16 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import styled from 'styled-components'
-import remcalc from 'remcalc'
 import { Row, Col } from 'react-styled-flexboxgrid'
 import { Padding } from 'styled-components-spacing'
-import breakpoint from 'styled-components-breakpoint'
-import Image from '../Common/Image'
 import StyledLink from '../styledLink'
 import SeoLinks from '../Common/seoLinks'
+import { AnimatedLink, CardHeader, PosterImage } from '../Common/animatedLink'
 import { H2, H4, H6, Paragraph } from '../Typography'
-
-const CardHeader = styled.header`
-  padding: ${remcalc(24)} ${remcalc(36)} ${remcalc(22)} ${remcalc(36)};
-  max-width: ${remcalc(475)};
-  box-sizing: border-box;
-
-  > div {
-    max-width: ${remcalc(310)};
-  }
-`
 
 const Title = styled(H4)`
   font-weight: 500;
 `
-
-const AnimatedLink = styled(Link)`
-  > section {
-    transition: all 250ms ease;
-  }
-
-  &:hover {
-    section {
-      transform: scale(0.97);
-    }
-  }
-`
-
-const PosterImage = styled.div`
-  background: #${props => props.color};
-
-  max-width: 100%;
-
-  ${breakpoint('tablet')`
-    height: 528px;
-  `}
-
-  ${breakpoint('desktop')`
-    width: 475px;
-    height: 473px;
-  `}
-`
-function isEven(value) {
+function isEven (value) {
   if (value % 2 === 0) return true
   else return false
 }
@@ -79,7 +39,7 @@ const Specialty = ({ services }) => (
         <AnimatedLink to={`/case-study/${service.caseStudies[0].slug}`}>
           <section
             style={{
-              background: `#${service.caseStudies[0].posterColor}`
+              background: `#${service.caseStudies[0].posterColor}`,
             }}
           >
             <CardHeader>
