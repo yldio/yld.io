07c53a279a9a231882c0b5f8cdd8397037b2106b
Zooey Miller <zooeyxmiller@gmail.com>
2018-11-22 13:00:24 +0000

    Sets up the environment for storyshot tests (#47)
    * initial storyshots / jest set up
    
    * add public to eslint ignore
    
    * separate snapshot tests and lighthouse
    
    * add snapshots to CI

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  transform: {
+    '^.+\\.jsx?$': '<rootDir>/.jest/jestPreprocess.js'
+  },
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/.jest/__mocks__/fileMock.js'
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules',
+    '<rootDir>/.cache',
+    '<rootDir>/public'
+  ],
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
+  globals: {
+    __PATH_PREFIX__: ''
+  },
+  testURL: 'http://localhost',
+  setupFiles: [
+    '<rootDir>/.jest/registerContext.js',
+    '<rootDir>/.jest/loaderShim.js'
+  ]
+}
