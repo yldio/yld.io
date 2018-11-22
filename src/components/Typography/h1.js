5c32a9647e17cc970d189cae6e48e33178d5a576
Sara Vieira <hey@iamsaravieira.com>
2018-11-22 16:07:12 +0100

    Update type scale (#50)
    https://trello.com/c/GcZ4qmku/127-update-typescale

--- src/components/Typography/h1.js
+++ src/components/Typography/h1.js
@@ -6,9 +6,9 @@ import is from 'styled-is'
 export default styled.h1`
   color: ${props => props.theme.colors.text};
   font-weight: 500;
-  font-size: ${remcalc(30)};
-  line-height: ${remcalc(30)};
-  padding: ${remcalc(29)} 0 ${remcalc(25)} 0;
+  font-size: ${remcalc(32)};
+  line-height: ${remcalc(36)};
+  padding: ${remcalc(13)} 0 ${remcalc(11)} 0;
 
   ${is('reverse')`
     color: ${props => props.theme.colors.white};
@@ -26,10 +26,10 @@ export default styled.h1`
       padding-top: 0;
   `};
 
-  ${breakpoint('tablet')`
-    font-size: ${remcalc(60)};
-    padding: ${remcalc(21)} 0;
-    line-height: ${remcalc(60)};
+  ${breakpoint('smallTablet')`
+    font-size: ${remcalc(42)};
+    padding: ${remcalc(10)} 0 ${remcalc(8)};
+    line-height: ${remcalc(48)};
 
       ${is('noTop')`
       padding-top: 0;
