d498a8d28a688ba467ee16aec8b9d1b2f135dff4
Jhonny Moreira <jhonny.moreira@yld.io>
2018-11-22 11:43:16 +0000

    Community Section

--- src/templates/speciality.js
+++ src/templates/speciality.js
@@ -60,32 +60,41 @@ const Specialty = ({ data }) => {
       </Helmet>
       <Grid className="grid">
         <Row>
-          <Col xs={12}>
+          <Col xs={12} sm={12} md={6}>
             <H1>{specialty.title}</H1>
             <Paragraph>
               {specialty.seoText.content[0].content[0].value}
             </Paragraph>
+          </Col>
+          <Col md={6} sm={12} xs={12}>
+            <img
+              alt={specialty.introGraphic.title}
+              src={specialty.introGraphic.file.url}
+              style={{ maxHeight: '100%' }}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} sm={12} xs={12}>
             <H2>{specialty.introTitle}</H2>
-            <Row>
-              <Col md={4} sm={12} xs={12}>
-                <H4>{specialty.introTextTitle1}</H4>
-                <Paragraph>
-                  {specialty.introTextBody1.content[0].content[0].value}
-                </Paragraph>
-              </Col>
-              <Col md={4} sm={12} xs={12}>
-                <H4>{specialty.introTextTitle2}</H4>
-                <Paragraph>
-                  {specialty.introTextBody2.content[0].content[0].value}
-                </Paragraph>
-              </Col>
-              <Col md={4} sm={12} xs={12}>
-                <H4>{specialty.introTextTitle3}</H4>
-                <Paragraph>
-                  {specialty.introTextBody3.content[0].content[0].value}
-                </Paragraph>
-              </Col>
-            </Row>
+          </Col>
+          <Col md={4} sm={12} xs={12}>
+            <H4>{specialty.introTextTitle1}</H4>
+            <Paragraph>
+              {specialty.introTextBody1.content[0].content[0].value}
+            </Paragraph>
+          </Col>
+          <Col md={4} sm={12} xs={12}>
+            <H4>{specialty.introTextTitle2}</H4>
+            <Paragraph>
+              {specialty.introTextBody2.content[0].content[0].value}
+            </Paragraph>
+          </Col>
+          <Col md={4} sm={12} xs={12}>
+            <H4>{specialty.introTextTitle3}</H4>
+            <Paragraph>
+              {specialty.introTextBody3.content[0].content[0].value}
+            </Paragraph>
           </Col>
         </Row>
       </Grid>
@@ -165,10 +174,21 @@ const Specialty = ({ data }) => {
         </Row>
       </Grid>
       <Grid className="grid">
-        <Col md={6} sm={12} xs={12}>
-          <H1>{specialty.title}</H1>
-          <H1 muted>related projects</H1>
-        </Col>
+        <Row>
+          <Col md={6} sm={12} xs={12}>
+            <img
+              alt={specialty.communityLogo.title}
+              src={specialty.communityLogo.file.url}
+              style={{ maxHeight: '100%' }}
+            />
+          </Col>
+          <Col md={6} sm={12} xs={12}>
+            <H1>{`${specialty.title} community`}</H1>
+            <Paragraph>
+              {specialty.communityText.content[0].content[0].value}
+            </Paragraph>
+          </Col>
+        </Row>
       </Grid>
     </Layout>
   )
@@ -198,6 +218,14 @@ export const pageQuery = graphql`
                 nodeType
                 value
               }
+            }
+          }
+          introGraphic {
+            id
+            title
+            file {
+              fileName
+              url
             }
           }
           introTitle
