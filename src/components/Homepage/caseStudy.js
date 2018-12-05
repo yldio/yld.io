f197918fdc55fee510689d354d6b776cb7e2496a
Zooey Miller <zooeyxmiller@gmail.com>
2018-12-05 16:52:22 +0000

    Case study preview tweaks (#61)
    ## Description - [Trello ticket](https://trello.com/c/WtCuaJT8/128-update-case-study-preview-component)
    
    updated the case study preview component to be close to design spec
    
    ## Review template
    
    The below is for reviewers of this PR to copy/paste into the review body and fulfill.
    
    - Old grid break points: 0-599, 600-1095, 1095+
    - New grid break points: 0-470, 471-552, 553-700, 701-899, 900-1196, 1197+
    
    I have opened the preview link and:
    
    - [ ] checked that this PR resolves the spec provided from trello / this description
    - [ ] checked the effected pages at all breakpoints
    - [ ] ensured that this PR does not introduce any obvious bugs

--- src/components/Homepage/caseStudy.js
+++ src/components/Homepage/caseStudy.js
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import remcalc from 'remcalc'
 import { Row, Col } from '../grid'
-import { Margin, Padding } from 'styled-components-spacing'
 import breakpoint from 'styled-components-breakpoint'
+import { Margin, Padding } from 'styled-components-spacing'
 import StyledLink from '../styledLink'
 import Image from '../Common/Image'
 import { H2, Paragraph } from '../Typography'
@@ -11,54 +11,68 @@ import getIntroSentence from '../../utils/getIntroSentence'
 
 const ImageWrapper = styled(Col)`
   margin-top: ${remcalc(-5)};
-  width: 100%;
+  /* width: 100%; */
   max-width: ${remcalc(549)};
   height: auto;
+  ${breakpoint('smallTablet')`
+    padding-right: 0;
+  `}
+
+  ${breakpoint('tablet')`
+    padding-left: 0;
+  `}
 
   ${breakpoint('desktop')`
     max-width: ${remcalc(549)};
-    padding-right: 0px;
   `}
 `
 
 const WrapperRow = styled(Row)`
-  ${breakpoint('tablet')`
-    min-height: ${remcalc(540)};
+  ${breakpoint('smallTablet')`
     align-items: center;
   `};
 `
 
 const P = styled(Paragraph)`
-  margin-bottom: ${remcalc(9)};
+  ${breakpoint('tablet')`
+    margin-top: ${remcalc(12)};
+  `}
 `
 
 const SubHeading = styled(Paragraph)`
   color: #757575;
-  padding-bottom: ${remcalc(15)};
+  padding-bottom: 0;
   opacity: 0.7;
+`
+
+const TextWrapper = styled.div`
+  ${breakpoint('desktop')`
+    width: 431px;
+    margin-left: auto;
+  `}
 `
 
 const CaseStudy = ({ caseStudy, subHeading }) => (
   <WrapperRow>
     <Col width={[1, 1, 1, 1, 0]}>
       <SubHeading>{subHeading}</SubHeading>
-      <Padding bottom={{ smallPhone: 0, phone: 0, tablet: 3, desktop: 1 }}>
+      <Padding bottom={3}>
         <H2>{caseStudy.title}</H2>
       </Padding>
-    </Col>
-    <Col width={[0, 0, 0, 0, 1 / 2]}>
-      <SubHeading>{subHeading}</SubHeading>
-      <Margin bottom={1}>
-        <H2>{caseStudy.title}</H2>
-      </Margin>
-      <P>{getIntroSentence(caseStudy)}</P>
-      <StyledLink to={`/case-study/${caseStudy.slug}`}>Learn more</StyledLink>
     </Col>
     <ImageWrapper width={[1, 1, 1, 1, 1 / 2]}>
       <Image alt={caseStudy.posterImage.title} image={caseStudy.posterImage} />
     </ImageWrapper>
+    <Col width={[0, 0, 0, 0, 1 / 2]}>
+      <TextWrapper>
+        <SubHeading>{subHeading}</SubHeading>
+        <H2 noTop>{caseStudy.title}</H2>
+        <P>{getIntroSentence(caseStudy)}</P>
+        <StyledLink to={`/case-study/${caseStudy.slug}`}>Learn more</StyledLink>
+      </TextWrapper>
+    </Col>
     <Col width={[1, 1, 1, 1, 0]}>
-      <Margin top={2} />
+      <Margin top={3} />
       <P>{getIntroSentence(caseStudy)}</P>
       <StyledLink
         aria-label={`Learn more about ${caseStudy.title}`}
