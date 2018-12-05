204ec0bb223d2ff09dea38492e7a6f77edd77dad
zooeymiller <zooeyxmiller@gmail.com>
2018-12-05 16:36:33 +0000

    case study preview mobile

--- src/components/Homepage/caseStudy.js
+++ src/components/Homepage/caseStudy.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import remcalc from 'remcalc'
 import { Row, Col } from '../grid'
 import breakpoint from 'styled-components-breakpoint'
+import { Margin, Padding } from 'styled-components-spacing'
 import StyledLink from '../styledLink'
 import Image from '../Common/Image'
 import { H2, Paragraph } from '../Typography'
@@ -13,7 +14,9 @@ const ImageWrapper = styled(Col)`
   /* width: 100%; */
   max-width: ${remcalc(549)};
   height: auto;
-  padding-right: 0;
+  ${breakpoint('smallTablet')`
+    padding-right: 0;
+  `}
 
   ${breakpoint('tablet')`
     padding-left: 0;
@@ -42,12 +45,6 @@ const SubHeading = styled(Paragraph)`
   opacity: 0.7;
 `
 
-const ImageCol = styled(Col)`
-  ${breakpoint('tablet')`
-    padding-left: 0;
-  `}
-`
-
 const TextWrapper = styled.div`
   ${breakpoint('desktop')`
     width: 431px;
@@ -57,18 +54,33 @@ const TextWrapper = styled.div`
 
 const CaseStudy = ({ caseStudy, subHeading }) => (
   <WrapperRow>
-    <ImageWrapper width={[1, 1, 1, 1, 1 / 2, 1 / 2]}>
+    <Col width={[1, 1, 1, 1, 0]}>
+      <SubHeading>{subHeading}</SubHeading>
+      <Padding bottom={3}>
+        <H2>{caseStudy.title}</H2>
+      </Padding>
+    </Col>
+    <ImageWrapper width={[1, 1, 1, 1, 1 / 2]}>
       <Image alt={caseStudy.posterImage.title} image={caseStudy.posterImage} />
     </ImageWrapper>
-    <ImageCol width={[1, 1, 1, 1 / 2, 1 / 2]}>
+    <Col width={[0, 0, 0, 0, 1 / 2]}>
       <TextWrapper>
         <SubHeading>{subHeading}</SubHeading>
         <H2 noTop>{caseStudy.title}</H2>
-
         <P>{getIntroSentence(caseStudy)}</P>
         <StyledLink to={`/case-study/${caseStudy.slug}`}>Learn more</StyledLink>
       </TextWrapper>
-    </ImageCol>
+    </Col>
+    <Col width={[1, 1, 1, 1, 0]}>
+      <Margin top={3} />
+      <P>{getIntroSentence(caseStudy)}</P>
+      <StyledLink
+        aria-label={`Learn more about ${caseStudy.title}`}
+        to={`/case-study/${caseStudy.slug}`}
+      >
+        Learn more
+      </StyledLink>
+    </Col>
   </WrapperRow>
 )
 
