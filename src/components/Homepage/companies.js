b89cea35a4db347c937e8e040156301b18c345eb
Sara Vieira <hey@iamsaravieira.com>
2018-12-10 16:34:20 +0100

    more fixs

--- src/components/Homepage/companies.js
+++ src/components/Homepage/companies.js
@@ -12,7 +12,7 @@ const Companies = ({ companies }) => (
   <Grid>
     <Row>
       {companies.map(company => (
-        <Column width={[1 / 2, 1 / 2, 1 / 3, 1 / 4, 1 / 4]} key={company.id}>
+        <Column width={[1 / 2, 1 / 2, 1 / 2, 1 / 4, 1 / 4]} key={company.id}>
           <img
             style={{ minWidth: 100 }}
             alt={company.title}
