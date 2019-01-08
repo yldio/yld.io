823705701fee019b402c98a39b9410d45a8babb0
Sara Vieira <hey@iamsaravieira.com>
2019-01-08 11:49:44 +0000

    Update src/components/Common/Hr.js
    add line break within styled component
    
    Co-Authored-By: jsms90 <jsms90@gmail.com>

--- src/components/Common/Hr.js
+++ src/components/Common/Hr.js
@@ -6,7 +6,8 @@ const Hr = styled.hr`
   height: ${remcalc(1)};
   margin: ${remcalc(17)} 0 ${remcalc(18)} 0;
   border: none;
-  background-color: ${props => props.theme.colors.grey} ${is('short')`
+  background-color: ${props => props.theme.colors.grey};
+  ${is('short')`
     width: 25%
   `} ${is('muted')`
     opacity: 0.25;
