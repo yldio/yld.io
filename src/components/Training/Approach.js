31ca98be6a144fd22c0ebe32802dbfdef4647ee2
Sara Vieira <hey@iamsaravieira.com>
2019-01-11 10:56:47 +0100

    mobile approach

--- src/components/Training/Approach.js
+++ src/components/Training/Approach.js
@@ -20,13 +20,18 @@ const Hr = styled.hr`
 
 const Approach = ({ title, content, formats }) => (
   <GrayBackground style={{ background: '#090329' }}>
-    <Padding top={{ smallPhone: 4 }} bottom={{ smallPhone: 4 }}>
+    <Padding
+      top={{ smallPhone: 3, tablet: 4 }}
+      bottom={{ smallPhone: 3, tablet: 4 }}
+    >
       <Grid>
         <Row>
           <Col width={[1, 1, 1, 1, 1 / 2]}>
-            <H2 style={{ maxWidth: 250 }} reverse>
-              Our training approach
-            </H2>
+            <Padding bottom={{ smallPhone: 3, tablet: 0 }}>
+              <H2 style={{ maxWidth: 250 }} reverse>
+                Our training approach
+              </H2>
+            </Padding>
           </Col>
           <Col width={[1, 1, 1, 1, 1 / 2]}>
             {title.map((approach, i) => (
@@ -41,7 +46,10 @@ const Approach = ({ title, content, formats }) => (
             ))}
           </Col>
         </Row>
-        <Padding top={{ smallPhone: 4 }} bottom={{ smallPhone: 4 }}>
+        <Padding
+          top={{ smallPhone: 3, tablet: 4 }}
+          bottom={{ smallPhone: 3, tablet: 4 }}
+        >
           <Hr />
         </Padding>
         <Row>
@@ -55,34 +63,36 @@ const Approach = ({ title, content, formats }) => (
       <Row>
         {formats.map(format => (
           <Col width={[1, 1, 1, 1, 1 / 3]} key={format.id}>
-            <Padding bottom={1}>
-              <img
-                src={`https://${format.icon.file.url}`}
-                alt={format.icon.title}
-              />
-            </Padding>
-            <Paragraph noMargin bold reverse>
-              {format.title}
-            </Paragraph>
-            <Paragraph muted reverse>
-              {format.description}
-            </Paragraph>
-            <Padding top={1} style={{ maxWidth: '80%' }}>
-              {format.bulletPoints.map((point, i) => (
-                <Padding top={1} bottom={1} key={i}>
-                  <Paragraph muted reverse>
-                    {point}
-                  </Paragraph>
-                  <Padding top={0.5}>
-                    <Hr small />
+            <Padding bottom={{ smallPhone: 3, tablet: 0 }}>
+              <Padding bottom={1}>
+                <img
+                  src={`https://${format.icon.file.url}`}
+                  alt={format.icon.title}
+                />
+              </Padding>
+              <Paragraph noMargin bold reverse>
+                {format.title}
+              </Paragraph>
+              <Paragraph muted reverse>
+                {format.description}
+              </Paragraph>
+              <Padding top={1} style={{ maxWidth: '80%' }}>
+                {format.bulletPoints.map((point, i) => (
+                  <Padding top={1} bottom={1} key={i}>
+                    <Paragraph muted reverse>
+                      {point}
+                    </Paragraph>
+                    <Padding top={0.5}>
+                      <Hr small />
+                    </Padding>
                   </Padding>
-                </Padding>
-              ))}
+                ))}
+              </Padding>
             </Padding>
           </Col>
         ))}
       </Row>
-      <Padding bottom={5} />
+      <Padding bottom={{ smallPhone: 0, tablet: 5 }} />
     </Grid>
   </GrayBackground>
 )
