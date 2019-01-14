9566817856aa056112dff4746f0c8b8259031aec
Jennifer Spencer <jennifer.spencer@yld.io>
2019-01-14 16:40:12 +0000

    put extra column back in on desktop

--- src/components/Common/CaseStudyCards/CaseStudyLayout.js
+++ src/components/Common/CaseStudyCards/CaseStudyLayout.js
@@ -15,21 +15,28 @@ const FlexCol = styled(Col)`
   display: flex;
 `
 
+const RowLayout = styled(Row)`
+  // creates extra column of space between image & text on RHS
+  ${breakpoint('desktop')`
+    justify-content: space-between;
+  `}
+`
+
 // headings & mainText should be sub-components (rather than a string, for example)
 const CaseStudyLayout = ({ posterImage, headings, mainText }) => (
-  <Row>
+  <RowLayout>
     <Col width={[1, 1, 1, 1, 0]}>{headings}</Col>
     <Col width={[1, 1, 1, 1, 1 / 2]} pb={[3, 3, 3, 3, 0]}>
       <Image alt={posterImage.title} image={posterImage} />
     </Col>
-    <FlexCol width={[0, 0, 0, 0, 1 / 2, 1 / 2]}>
+    <FlexCol px={[0, 0, 0, 0, 0, 0, 0]} width={[0, 0, 0, 0, 1 / 2, 5 / 12]}>
       <TextWrapper>
         <Padding bottom={0.5}>{headings}</Padding>
         <Padding bottom={1}>{mainText}</Padding>
       </TextWrapper>
     </FlexCol>
     <Col width={[1, 1, 1, 1, 0]}>{mainText}</Col>
-  </Row>
+  </RowLayout>
 )
 
 export default CaseStudyLayout
