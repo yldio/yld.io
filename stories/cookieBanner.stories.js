3d5e4ce985f9681de00f7f0497b319d7fb802a15
Sara Vieira <hey@iamsaravieira.com>
2019-01-22 14:17:46 +0100

    add snapshots

---
+++ stories/cookieBanner.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf, addDecorator } from '@storybook/react'
+import Theme from './theme'
+import CookieBanner from '../src/components/Common/CookieBanner'
+
+addDecorator(Theme)
+
+storiesOf('CookieBanner', module).add('CookieBanner', () => <CookieBanner />)
