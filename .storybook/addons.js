0c33811a217ce24fb573141cab345193c0e155a9
Capelo <antonio.c.capelo@gmail.com>
2019-02-01 14:27:30 +0000

    chore: remove storybook viewport

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,8 +1,8 @@
 import '@storybook/addon-actions/register'
 import '@storybook/addon-links/register'
 import '@storybook/addon-a11y/register'
-import '@storybook/addon-viewport/register'
 import registerScissors from 'storybook-addon-scissors'
+
 const devices = [
   {
     uid: 'small-mobile',
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { configure, addDecorator } from '@storybook/react'
 import { checkA11y } from '@storybook/addon-a11y'
-import { configureViewport } from '@storybook/addon-viewport'
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/)
@@ -24,4 +23,3 @@ window.___navigate = pathname => {
 
 configure(loadStories, module)
 addDecorator(checkA11y)
-configureViewport()
