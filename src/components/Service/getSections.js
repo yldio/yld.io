95410fe8417a759365f737a50008f983436ac602
Sara Vieira <hey@iamsaravieira.com>
2019-02-05 18:48:47 +0100

    mr comments

--- src/components/Service/getSections.js
+++ src/components/Service/getSections.js
@@ -1,10 +1,9 @@
 const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1)
 
-export default workStage => {
-  const lol = Array(15)
-    .fill({})
+export default workStage =>
+  Array.from(new Array(15))
     .map((_, index) => {
-      // get the current body of the part e want. They all numbered starting with 1.
+      // get the current body of the part we want. They're all numbered starting with 1.
       // arrays start with 0 so that's why the + 1
       const getCurrent = part => `section${capitalize(part)}${index + 1}`
       return {
@@ -15,5 +14,3 @@ export default workStage => {
       }
     })
     .filter(({ title }) => title)
-  return lol
-}
