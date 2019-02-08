646ea679c69db776fc97561d28fc3dd7c2c944de
Sara Vieira <hey@iamsaravieira.com>
2019-02-08 12:47:35 +0100

    works yay

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import { Row, Col, Grid } from '../grid'
 import Flex from 'styled-flex-component'
@@ -15,72 +15,66 @@ import {
   Overlay
 } from './elements.js'
 
-class Header extends Component {
-  state = { menuOpen: false }
+const Header = ({ path, blue, logoColour }) => {
+  const [menuOpen, toggleMenu] = useState(false)
 
-  toggleMenu = () => this.setState(state => ({ menuOpen: !state.menuOpen }))
-
-  render() {
-    const { path, blue, logoColour } = this.props
-
-    return (
-      <Grid>
-        <Row>
-          <Col width={[1]}>
-            <header>
-              <Padding top={2} bottom={3}>
-                <Flex alignCenter wrap justifyBetween>
-                  <Link to="/">
-                    <Logo path={path} blue={blue} logoColour={logoColour} />
+  return (
+    <Grid>
+      <Row>
+        <Col width={[1]}>
+          <header>
+            <Padding top={2} bottom={3}>
+              <Flex alignCenter wrap justifyBetween>
+                <Link to="/">
+                  <Logo path={path} blue={blue} logoColour={logoColour} />
+                </Link>
+                <MobileMenu>
+                  <button onClick={() => toggleMenu(!menuOpen)}>
+                    <img src={menu} alt="open menu" />
+                  </button>
+                </MobileMenu>
+                <DesktopMenu open={menuOpen}>
+                  <Close onClick={() => toggleMenu(!menuOpen)}>
+                    <img src={close} alt="Close menu" />
+                  </Close>
+                  <HomeLink right={30}>
+                    <Link activeClassName="active" to="/">
+                      Home
+                    </Link>
+                  </HomeLink>
+                  <Link reverse activeClassName="active" to="/engineering/">
+                    Engineering
                   </Link>
-                  <MobileMenu>
-                    <button onClick={this.toggleMenu}>
-                      <img src={menu} alt="open menu" />
-                    </button>
-                  </MobileMenu>
-                  <DesktopMenu open={this.state.menuOpen}>
-                    <Close onClick={this.toggleMenu}>
-                      <img src={close} alt="Close menu" />
-                    </Close>
-                    <HomeLink right={30}>
-                      <Link activeClassName="active" to="/">
-                        Home
-                      </Link>
-                    </HomeLink>
-                    <Link reverse activeClassName="active" to="/engineering/">
-                      Engineering
-                    </Link>
-                    <Link activeClassName="active" to="/design/">
-                      Design
-                    </Link>
-                    <Link activeClassName="active" to="/training/">
-                      Training
-                    </Link>
-                    <Link activeClassName="active" to="/our-work/">
-                      Our work
-                    </Link>
-                    <Link activeClassName="active" to="/contact/">
-                      Contact
-                    </Link>
-                    <ExternalAnchor href="https://medium.com/yld-engineering-blog/">
-                      Blog
-                    </ExternalAnchor>
-                    <Link activeClassName="active" to="/join-us/">
-                      Join Us
-                    </Link>
-                  </DesktopMenu>
-                  <Overlay
-                    visible={this.state.menuOpen}
-                    onClick={this.toggleMenu}
-                  />
-                </Flex>
-              </Padding>
-            </header>
-          </Col>
-        </Row>
-      </Grid>
-    )
-  }
+                  <Link activeClassName="active" to="/design/">
+                    Design
+                  </Link>
+                  <Link activeClassName="active" to="/training/">
+                    Training
+                  </Link>
+                  <Link activeClassName="active" to="/our-work/">
+                    Our work
+                  </Link>
+                  <Link activeClassName="active" to="/contact/">
+                    Contact
+                  </Link>
+                  <ExternalAnchor href="https://medium.com/yld-engineering-blog/">
+                    Blog
+                  </ExternalAnchor>
+                  <Link activeClassName="active" to="/join-us/">
+                    Join Us
+                  </Link>
+                </DesktopMenu>
+                <Overlay
+                  visible={menuOpen}
+                  onClick={() => toggleMenu(!menuOpen)}
+                />
+              </Flex>
+            </Padding>
+          </header>
+        </Col>
+      </Row>
+    </Grid>
+  )
 }
 
 Header.defaultProps = {
