77409d9a72a41b2de16e28f4dad234cd5e440e89
Capelo <antonio.c.capelo@gmail.com>
2019-02-18 12:15:35 +0000

    fix: fix color on mobile menu

--- src/components/Header/HeaderAnchor.js
+++ src/components/Header/HeaderAnchor.js
@@ -20,6 +20,7 @@ const HeaderAnchor = styled(Anchor)`
   transition-property: opacity, color, outline;
   padding: 0;
   background: linear-gradient(to right, #616161 0%, transparent 0);
+  color: ${props => props.theme.colors.white};
 
   /* Styles for mobile */
   opacity: 0.5;
@@ -30,7 +31,7 @@ const HeaderAnchor = styled(Anchor)`
 
   &:hover, &.active {
     color: ${props => props.theme.colors.white};
-    opacity: 1; 
+    opacity: 1;
   }
 
   ${breakpoint('phone')`
