583b0d82d315a0c87fb5175137f21a3231bbbda1
Jennifer Spencer <jennifer.spencer@yld.io>
2019-02-18 14:51:02 +0000

    add graphql query to Contentful for page content

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,24 +1,75 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import Head from '../components/Common/Head'
 
-const AboutUs = ({ content = mockAboutUsContent }) => (
-  <Layout backgroundColor="grey">
+const AboutUs = ({ data: { contentfulAboutUsPage: content } }) => (
+  <Layout>
     <Head page={content} />
   </Layout>
 )
 
-const mockAboutUsContent = {
-  site: {
-    siteMetaData: {
-      title: 'YLD | About Us'
-    }
-  },
-  contentfulAboutUsPage: {
-    title: 'YLD | About Us',
-    seoTitle: 'YLD | About Us'
-  }
-}
+const AboutUsPage = props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        contentfulAboutUsPage {
+          title
+          seoTitle
+          seoDescription
+          teamSectionTitle
+          teams {
+            name
+            members {
+              name
+              role
+              image {
+                title
+                file {
+                  url
+                }
+              }
+              socialLinks {
+                name
+                url
+                image {
+                  title
+                  file {
+                    url
+                  }
+                }
+              }
+            }
+          }
+          yldGroupTitle
+          subsidiaries {
+            name
+            linkUrl
+            linkText
+          }
+          clientTestimonialTitle
+          clientTestimonialVideo {
+            title
+            type
+            link
+          }
+          partnershipsTitle
+          partners {
+            name
+            url
+            image {
+              title
+              file {
+                url
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => <AboutUs data={data} {...props} />}
+  />
+)
 
-export default AboutUs
+export default AboutUsPage
