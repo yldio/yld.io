93035fa5d82d3857e8541a5fa11086c2c6b8cb21
Capelo <antonio.c.capelo@gmail.com>
2019-02-20 10:46:19 +0000

    feat: about us page - teams section
    * feat: about us page - teams section
    
    * feat: add fluid data for images
    
    * fix: change image width queries
    
    * fix: small adjustment to tab spacing on small devices
    
    * fix: destructuring for consistency
    
    * fix: code review
    
    * update snapshots
    
    * fix: remove unneeded defensive code

---
+++ src/components/AboutUs/Teams.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import { Padding } from 'styled-components-spacing'
+
+import { Row, Col, Grid } from '../grid'
+import { SectionTitle } from '../Typography'
+import Tab, { Tabs } from '../Common/Tab'
+import GreyBackground from '../Common/GreyBackground'
+import StaffCard from './StaffCard'
+
+const Team = ({ members }) => {
+  return (
+    <Padding top={{ smallPhone: 3, tablet: 4 }}>
+      <Row>
+        {(members || []).map(
+          ({ name, description, role, image, socialLinks }, idx) => (
+            <StaffCard
+              key={`staff-${name}-${idx}`}
+              name={name}
+              description={description.description}
+              role={role}
+              image={image}
+              socialLinks={socialLinks}
+            />
+          )
+        )}
+      </Row>
+    </Padding>
+  )
+}
+
+const Teams = ({ title, teams }) => {
+  const [currentTab, setCurrentTab] = useState(0)
+  const currentTeam = teams[currentTab]
+
+  return (
+    <GreyBackground>
+      <Grid>
+        <Padding
+          top={{ smallPhone: 3, tablet: 4 }}
+          bottom={{ smallPhone: 1.5, smallTablet: 0 }}
+        >
+          <Padding bottom={2}>
+            <SectionTitle>{title}</SectionTitle>
+          </Padding>
+          <Row>
+            <Col width={1}>
+              <Tabs>
+                {(teams || []).map((team, idx) => (
+                  <Tab
+                    key={idx}
+                    active={currentTab === idx}
+                    onClick={() => setCurrentTab(idx)}
+                  >
+                    {team.name}
+                  </Tab>
+                ))}
+              </Tabs>
+            </Col>
+            <Col />
+          </Row>
+          <Team members={currentTeam.members} />
+        </Padding>
+      </Grid>
+    </GreyBackground>
+  )
+}
+
+export default Teams
