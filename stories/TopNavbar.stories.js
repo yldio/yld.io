fb3e856bfa5adaeee95e5a320480d3fa63c46646
Jennifer Spencer <jennifer.spencer@yld.io>
2019-02-28 16:19:39 +0000

    add stories for TopNav with dropdown & overall Header component

--- stories/TopNavbar.stories.js
+++ stories/TopNavbar.stories.js
@@ -5,7 +5,7 @@ import TopNavbar from '../src/components/Header/TopNavbar'
 
 addDecorator(Theme)
 
-const links = [
+const anchors = [
   {
     text: 'Home',
     path: '/',
@@ -23,6 +23,58 @@ const links = [
   }
 ]
 
-storiesOf('Header', module).add('TopNavbar - light theme', () => (
-  <TopNavbar links={links} dark={false} />
-))
+const anchorsAndDropdowns = [
+  {
+    text: 'Services',
+    dropdownItems: [
+      {
+        label: 'Engineering',
+        to: '/engineering/'
+      },
+      {
+        label: 'Design',
+        to: '/design/'
+      },
+      {
+        label: 'Training',
+        to: '/training/'
+      }
+    ]
+  },
+  {
+    text: 'Our work',
+    path: '/our-work/',
+    isInternal: true
+  },
+  {
+    text: 'Blog',
+    path: 'https://medium.com/yld-engineering-blog/',
+    isInternal: false
+  },
+  {
+    text: 'About',
+    dropdownItems: [
+      {
+        label: 'Our team',
+        to: '/about-us/'
+      },
+      {
+        label: 'Contact',
+        to: '/contact/'
+      }
+    ]
+  },
+  {
+    text: 'Join Us',
+    path: '/join-us/',
+    isInternal: true
+  }
+]
+
+storiesOf('Header', module)
+  .add('TopNavbar - light theme', () => (
+    <TopNavbar links={anchors} dark={false} />
+  ))
+  .add('TopNavbar with dropdowns - light theme', () => (
+    <TopNavbar links={anchorsAndDropdowns} dark={false} />
+  ))
