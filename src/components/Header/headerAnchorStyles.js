3688363f278369e19d88985b7b66a8cfa0e95837
Jennifer Spencer <jennifer.spencer@yld.io>
2019-03-06 15:48:38 +0000

    speed up animations

--- src/components/Header/headerAnchorStyles.js
+++ src/components/Header/headerAnchorStyles.js
@@ -3,9 +3,9 @@ import remcalc from 'remcalc'
 
 const headerAnchorStyles = css`
   padding: ${remcalc(10)} ${remcalc(15)} ${remcalc(14)};
-  transition: opacity ${props => props.theme.animations.normal} ease-out,
+  transition: opacity ${props => props.theme.animations.fast} ease-out,
     color ${props => props.theme.animations.fast} ease-out,
-    outline ${props => props.theme.animations.normal} ease-out;
+    outline ${props => props.theme.animations.fast} ease-out;
   background: linear-gradient(to right, #616161 0%, transparent 0);
   outline: none;
 `
