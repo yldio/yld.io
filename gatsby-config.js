fa88ed407460b754d214bce3dda145db3b239e45
Joao Almeida <joao.almeida@thetrainline.com>
2019-03-11 17:13:51 +0000

    SW configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,64 @@ module.exports = {
         gtmAuth: GTM_AUTH,
         gtmPreview: 'env-2'
       }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        include_favicon: true // Include favicon
+      }
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        runtimeCaching: [
+          {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
+            urlPattern: /(\.js$|\.css$|static\/)/,
+            handler: `cacheFirst`
+          },
+          {
+            // Add runtime caching of various other page resources
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `staleWhileRevalidate`
+          },
+          {
+            // Google Fonts CSS (doesn't end in .css so we need to specify it)
+            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+            handler: `staleWhileRevalidate`
+          },
+          {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
+            urlPattern: /contact/,
+            handler: `networkOnly`,
+            options: {
+              backgroundSync: {
+                name: 'my-queue-name',
+                options: {
+                  maxRetentionTime: 60 * 60
+                }
+              }
+            },
+            method: 'POST'
+          },
+          {
+            // Google Fonts CSS (doesn't end in .css so we need to specify it)
+            urlPattern: /^https?:\/\/images\.ctfassets\.net/,
+            handler: `staleWhileRevalidate`
+          }
+        ]
+      }
     }
   ]
 }
