f7d406cdc0ca191c44cfc380ee222262c5b88e28
Jen Spencer <jsms90@gmail.com>
2019-03-13 12:28:53 +0000

    Fix case study component paddings (#149)
    * make padding on case study cards consistent across pages - apply padding to CaseStudyLayout
    and make Hero and Preview components sections in their own right (i.e. using Grid internally)
    
    * show null if/when caseStudy prop is not passed to CaseStudyPreview
    
    * fix CaseStudyPreviews on Training page too
    
    * fix top and bottom padding for case study cards used at the top of pages
    
    * remove top padding on CaseStudyLayout - now the only space between case study cards & the navbar is the padding on the nav itself

--- src/components/Common/CaseStudyCards/CaseStudyLayout.js
+++ src/components/Common/CaseStudyCards/CaseStudyLayout.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import breakpoint from 'styled-components-breakpoint'
-import { Row, Col } from '../../grid'
+import remcalc from 'remcalc'
+
+import { Grid, Row, Col } from '../../grid'
 import Image from '../Image'
 
 const TextWrapper = styled.div`
@@ -15,6 +17,18 @@ const FlexCol = styled(Col)`
 `
 
 const RowLayout = styled(Row)`
+  ${breakpoint('smallPhone')`
+    padding-top: ${props => (props.isTop ? 0 : props.theme.spacing[3.5])};
+    padding-bottom: ${props =>
+      props.isTop ? props.theme.spacing[3] : props.theme.spacing[3.5]};
+  `}
+
+  ${breakpoint('tablet')`
+    padding-top: ${props => (props.isTop ? 0 : props.theme.spacing[5])};
+    padding-bottom: ${props =>
+      props.isTop ? remcalc(86) : props.theme.spacing[5]};
+  `}
+
   /* creates extra column of space between image & text on RHS */
   ${breakpoint('desktop')`
     justify-content: space-between;
@@ -23,21 +37,24 @@ const RowLayout = styled(Row)`
 
 // headings, textBelowImage & rightHandText should be sub-components (rather than a string, for example)
 const CaseStudyLayout = ({
+  isTop = true,
   posterImage,
   headings,
   textBelowImage,
   rightHandText
 }) => (
-  <RowLayout>
-    <Col width={[1, 1, 1, 1, 0]}>{headings}</Col>
-    <Col width={[1, 1, 1, 1, 1 / 2]} pb={[3, 3, 3, 3, 0]}>
-      <Image alt={posterImage.title} image={posterImage} width="100%" />
-    </Col>
-    <FlexCol width={[0, 0, 0, 0, 1 / 2, 1 / 2, 5 / 12]}>
-      <TextWrapper>{rightHandText}</TextWrapper>
-    </FlexCol>
-    <Col width={[1, 1, 1, 1, 0]}>{textBelowImage}</Col>
-  </RowLayout>
+  <Grid>
+    <RowLayout isTop={isTop}>
+      <Col width={[1, 1, 1, 1, 0]}>{headings}</Col>
+      <Col width={[1, 1, 1, 1, 1 / 2]} pb={[3, 3, 3, 3, 0]}>
+        <Image alt={posterImage.title} image={posterImage} width="100%" />
+      </Col>
+      <FlexCol width={[0, 0, 0, 0, 1 / 2, 1 / 2, 5 / 12]}>
+        <TextWrapper>{rightHandText}</TextWrapper>
+      </FlexCol>
+      <Col width={[1, 1, 1, 1, 0]}>{textBelowImage}</Col>
+    </RowLayout>
+  </Grid>
 )
 
 export default CaseStudyLayout
