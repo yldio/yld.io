8e5b97f0515a4297a188dcb99c01e3e5d0f5fab8
Jennifer Spencer <jennifer.spencer@yld.io>
2019-04-02 14:43:30 +0100

    separate test server and running of lighthouse tests, then use test server for e2e tests as well

--- tests/e2e/training-page-test.js
+++ tests/e2e/training-page-test.js
@@ -1,20 +1,21 @@
+import createServer from '../createServer'
 import { Selector, ClientFunction } from 'testcafe'
 require('dotenv').config()
 
-const { GATSBY_ENVIRONMENT } = process.env
-
-const baseUrl =
-  GATSBY_ENVIRONMENT === 'preview'
-    ? process.env.DEPLOY_PRIME_URL
-    : process.env.BASE_URL
-
+const baseUrl = `localhost:3001`
 const trainingPageUrl = `${baseUrl}/training`
 const getWindowLocation = ClientFunction(() => window.location)
 
+let server
 let firstModalLink
-fixture`Training page`.page`${trainingPageUrl}`.beforeEach(async t => {
-  firstModalLink = await Selector('a[data-testid^="modal"]').nth(0)
-})
+fixture`Training page`.page`${trainingPageUrl}`
+  .before(async t => {
+    server = createServer()
+  })
+  .beforeEach(async t => {
+    firstModalLink = await Selector('a[data-testid^="modal"]').nth(0)
+  })
+  .after(() => server.close())
 
 test('should open the correct training course modal when accessed via a link', async t => {
   const coursePathName = await firstModalLink
---
+++ tests/lighthouse/launchChromeAndRunLighthouse.js
@@ -0,0 +1,17 @@
+const createServer = require('../createServer')
+const lighthouse = require('lighthouse')
+const chromeLauncher = require('chrome-launcher')
+const config = require('./lh-config.js')
+
+const launchChromeAndRunLighthouse = url => {
+  const server = createServer()
+
+  return chromeLauncher.launch({ chromeFlags: [] }).then(chrome => {
+    return lighthouse(url, { port: chrome.port }, config).then(results => {
+      server.close()
+      return chrome.kill().then(() => results)
+    })
+  })
+}
+
+module.exports = launchChromeAndRunLighthouse
