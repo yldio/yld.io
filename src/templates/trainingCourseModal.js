97414dfcedbc7eff179862c1979bd5316a334a80
Jennifer Spencer <jennifer.spencer@yld.io>
2019-04-03 15:43:58 +0100

    remove 'visible' prop from modal wrapper & remove separate CourseWrapper component

--- src/templates/trainingCourseModal.js
+++ src/templates/trainingCourseModal.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 import { graphql } from 'gatsby'
 import { Padding } from 'styled-components-spacing'
 import { ModalRoutingContext } from 'gatsby-plugin-modal-routing'
 
-import CourseWrapper from '../components/Training/course/CourseWrapper'
+import Layout from '../components/layout'
 import CourseInfo from '../components/Training/course/CourseInfo'
 import CourseContent from '../components/Training/course/CourseContent'
 import ModalCloseButton from '../components/Training/course/ModalCloseButton'
 import { Grid, Row, Col } from '../components/grid'
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${props => props.theme.colors.white};
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: ${props => props.theme.zIndexes.modal};
+  overflow: scroll;
+`
 
 const TrainingCourseModal = ({
   data: {
@@ -18,30 +30,32 @@ const TrainingCourseModal = ({
 }) => (
   <ModalRoutingContext.Consumer>
     {({ modal, closeTo }) => (
-      <CourseWrapper location={location} content={course}>
-        <Padding top={{ smallPhone: 5 }} bottom={{ smallPhone: 5 }}>
-          <ModalCloseButton
-            to={modal ? `${closeTo}/#${category.slug}` : `/training`}
-          />
-          <Grid>
-            <Row>
-              <Col width={[1, 1, 1, 1, 1 / 2]}>
-                <CourseInfo
-                  name={course.name}
-                  description={course.description.description}
-                  level={course.level}
-                  preRequisites={course.preRequisites}
-                  preRequisitesCourses={course.preRequisitesCourses}
-                  image={category.logo}
-                />
-              </Col>
-              <Col width={[1, 1, 1, 1, 1 / 2]}>
-                <CourseContent content={course.content.content} />
-              </Col>
-            </Row>
-          </Grid>
-        </Padding>
-      </CourseWrapper>
+      <Layout location={location}>
+        <Wrapper>
+          <Padding top={{ smallPhone: 5 }} bottom={{ smallPhone: 5 }}>
+            <ModalCloseButton
+              to={modal ? `${closeTo}/#${category.slug}` : `/training`}
+            />
+            <Grid>
+              <Row>
+                <Col width={[1, 1, 1, 1, 1 / 2]}>
+                  <CourseInfo
+                    name={course.name}
+                    description={course.description.description}
+                    level={course.level}
+                    preRequisites={course.preRequisites}
+                    preRequisitesCourses={course.preRequisitesCourses}
+                    image={category.logo}
+                  />
+                </Col>
+                <Col width={[1, 1, 1, 1, 1 / 2]}>
+                  <CourseContent content={course.content.content} />
+                </Col>
+              </Row>
+            </Grid>
+          </Padding>
+        </Wrapper>
+      </Layout>
     )}
   </ModalRoutingContext.Consumer>
 )
