60ea0f9fbd0d2d5570ff8a8bf328c4fef79dd026
Jennifer Spencer <jennifer.spencer@yld.io>
2019-04-08 15:36:38 +0100

    fix spacing between address blocks within Footer

--- src/components/Footer/office.js
+++ src/components/Footer/office.js
@@ -11,17 +11,19 @@ const StyledBodyPrimary = styled(BodyPrimary).attrs({ noPadding: true })`
 
 const Office = ({ name, telephone, email, streetAddress }) => (
   <Col width={[1, 1, 1, 1 / 2, 1 / 2, 1 / 4]}>
-    <Subtitle reverse>{name}</Subtitle>
-    <Padding top={1} bottom={1}>
-      {streetAddress.map((address, i) => (
-        <StyledBodyPrimary key={address}>{address}</StyledBodyPrimary>
-      ))}
-      <StyledBodyPrimary itemProp="telephone">{telephone}</StyledBodyPrimary>
-      {email ? (
-        <StyledBodyPrimary>
-          <a href={`mailto:${email}`}>{email}</a>
-        </StyledBodyPrimary>
-      ) : null}
+    <Padding bottom={3}>
+      <Subtitle reverse>{name}</Subtitle>
+      <Padding top={1} bottom={1}>
+        {streetAddress.map((address, i) => (
+          <StyledBodyPrimary key={address}>{address}</StyledBodyPrimary>
+        ))}
+        <StyledBodyPrimary itemProp="telephone">{telephone}</StyledBodyPrimary>
+        {email ? (
+          <StyledBodyPrimary>
+            <a href={`mailto:${email}`}>{email}</a>
+          </StyledBodyPrimary>
+        ) : null}
+      </Padding>
     </Padding>
   </Col>
 )
