543550d3bc381969458f830cab4e79863df0009b
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-04-12 16:36:23 +0100

    Compare node dates as dates format and sort after the filter

--- src/components/Homepage/events/getEvents.js
+++ src/components/Homepage/events/getEvents.js
@@ -1,10 +1,12 @@
 import { format, isAfter } from 'date-fns'
 
+const toDate = stringDate => new Date(stringDate)
+
 export const getHomepageMeetups = events =>
   events
-    .sort((a, b) => (a.node.date < b.node.date ? -1 : 1))
     .filter(n => !n.node.homepageFeatured)
     .filter(n => isAfter(new Date(n.node.date), new Date()))
+    .sort((a, b) => (toDate(a.node.date) < toDate(b.node.date) ? -1 : 1))
     .slice(0, 3)
     .reverse()
     .map(n => n.node)
