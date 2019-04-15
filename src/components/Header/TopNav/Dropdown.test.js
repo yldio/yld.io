862a71e83b3ed99ed217d35ca8785cdacda94399
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-04-15 12:56:13 +0100

    TopNav Dropdown - should render all dropdown items labels when passed as children

--- src/components/Header/TopNav/Dropdown.test.js
+++ src/components/Header/TopNav/Dropdown.test.js
@@ -4,18 +4,46 @@ import theme from '../../../utils/theme'
 import { ThemeProvider } from 'styled-components'
 import Dropdown from './Dropdown'
 
+const items = [
+  {
+    to: 'YLD Website',
+    href: 'https://www.yld.io/',
+    label: 'YLD'
+  },
+  {
+    to: 'ReactJS Girls website',
+    href: 'https://reactjsgirls.com/',
+    label: 'ReactJS Girls'
+  }
+]
+
+const dropdown = {
+  title: 'title',
+  labels: items.reduce((str, item) => str + item.label, '')
+}
+
 describe('TopNav Dropdown', () => {
   it('should render a dropdown menu title when passed as child', () => {
-    const expectedTitle = 'title'
-
     const container = document.createElement('div')
     ReactDOM.render(
       <ThemeProvider theme={theme}>
-        <Dropdown items={[]}>{expectedTitle}</Dropdown>
+        <Dropdown items={[]}>{dropdown.title}</Dropdown>
       </ThemeProvider>,
       container
     )
 
-    expect(container.textContent).toMatch(expectedTitle)
+    expect(container.textContent).toMatch(dropdown.title)
+  })
+
+  it('should render all dropdown items labels when passed as children', () => {
+    const container = document.createElement('div')
+    ReactDOM.render(
+      <ThemeProvider theme={theme}>
+        <Dropdown items={items}>{dropdown.title}</Dropdown>
+      </ThemeProvider>,
+      container
+    )
+
+    expect(container.textContent).toMatch(dropdown.labels)
   })
 })
