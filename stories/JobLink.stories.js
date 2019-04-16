908485ac09d5fafc3f78da45d3aaec1ac2546026
Jen Spencer <jsms90@gmail.com>
2019-04-16 16:30:52 +0100

    use same JobLink component on homepage & Join Us page (entire component is clickable) (#186)
    * move Join Us page version of JobLink into Common & use this on homepage - entire component is a clickable link
    
    * remove fixed height on JobLink
    
    * fix accessibility errors on lighthouse - JobLink is not an 'li'

---
+++ stories/JobLink.stories.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { storiesOf, addDecorator } from '@storybook/react'
+import Theme from './theme'
+import JobLink from '../src/components/Common/JobLink'
+
+addDecorator(Theme)
+
+storiesOf('JobLink', module).add('JobLink', () => {
+  return (
+    <JobLink
+      position="Software Engineer"
+      hostedUrl="http://www.lever.com"
+      contractType="Permanent"
+    />
+  )
+})
