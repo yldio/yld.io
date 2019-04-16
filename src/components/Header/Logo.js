1f54a9bbae4e1863cb43ab1def071a6fba875a27
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-04-16 11:02:29 +0100

    Replace the LogoEngComponents with simple Gatsby links

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -4,22 +4,22 @@ import styled from 'styled-components'
 
 import outlineStyles from './outlineStyles'
 import logo from '../../images/logo_animated.gif'
-import logoDesign from '../../images/design-logo.svg'
-import LogoEngComponent from '../../images/eng-logo-component'
-import logoTraining from '../../images/training-logo.svg'
+// import logoDesign from '../../images/design-logo.svg'
+// import LogoEngComponent from '../../images/eng-logo-component'
+// import logoTraining from '../../images/training-logo.svg'
 
-const HiddenText = styled.h1`
-  position: absolute;
-  top: -9999px;
-  left: -9999px;
-`
+// const HiddenText = styled.h1`
+//   position: absolute;
+//   top: -9999px;
+//   left: -9999px;
+// `
 
 const StyledLink = styled(Link)`
   ${outlineStyles}
 `
 
 const Logo = ({ path, logoColour, blue }) => {
-  const name = path.split('/').join('')
+  // const name = path.split('/').join('')
   return (
     <Fragment>
       <StyledLink to="/">
@@ -27,41 +27,44 @@ const Logo = ({ path, logoColour, blue }) => {
       </StyledLink>
 
       {path.includes('engineering') || blue ? (
-        <Fragment>
-          <HiddenText>{name}</HiddenText>
-          <LogoEngComponent
-            style={{ display: 'block' }}
-            boxColour={logoColour}
-            backgroundBlue={blue}
-          />
-        </Fragment>
-      ) : null}
+        <Link to="/engineering">Link to engineering</Link>
+      ) // <Fragment>
+      //   <HiddenText>{name}</HiddenText>
+      //   <LogoEngComponent
+      //     style={{ display: 'block' }}
+      //     boxColour={logoColour}
+      //     backgroundBlue={blue}
+      //   />
+      // </Fragment>
+      : null}
 
       {path.includes('training') ? (
-        <Fragment>
-          <HiddenText>{name}</HiddenText>
-          <img
-            role="link"
-            height="48"
-            src={logoTraining}
-            alt="yld training logo"
-            style={{ display: 'block' }}
-          />
-        </Fragment>
-      ) : null}
+        <Link to="/training">Link to training</Link>
+      ) // <Fragment>
+      //   <HiddenText>{name}</HiddenText>
+      //   <img
+      //     role="link"
+      //     height="48"
+      //     src={logoTraining}
+      //     alt="yld training logo"
+      //     style={{ display: 'block' }}
+      //   />
+      // </Fragment>
+      : null}
 
       {path.includes('design') ? (
-        <Fragment>
-          <HiddenText>{name}</HiddenText>
-          <img
-            role="link"
-            height="48"
-            src={logoDesign}
-            alt="yld design logo"
-            style={{ display: 'block' }}
-          />
-        </Fragment>
-      ) : null}
+        <Link to="/design">Link to design</Link>
+      ) // <Fragment>
+      //   <HiddenText>{name}</HiddenText>
+      //   <img
+      //     role="link"
+      //     height="48"
+      //     src={logoDesign}
+      //     alt="yld design logo"
+      //     style={{ display: 'block' }}
+      //   />
+      // </Fragment>
+      : null}
     </Fragment>
   )
 }
