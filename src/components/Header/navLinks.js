7514f87044e88de95838797792c4e0fcaf04e5eb
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-04-29 15:34:01 +0100

    Move navLinks in a file

---
+++ src/components/Header/navLinks.js
@@ -0,0 +1,46 @@
+const navLinks = [
+  {
+    label: 'Services',
+    dropdownItems: [
+      {
+        label: 'Engineering',
+        to: '/engineering/'
+      },
+      {
+        label: 'Design',
+        to: '/design/'
+      },
+      {
+        label: 'Training',
+        to: '/training/'
+      }
+    ]
+  },
+  {
+    label: 'Our work',
+    to: '/our-work/'
+  },
+  {
+    label: 'Blog',
+    href: 'https://medium.com/yld-engineering-blog/'
+  },
+  {
+    label: 'About',
+    dropdownItems: [
+      {
+        label: 'Our team',
+        to: '/about-us/'
+      },
+      {
+        label: 'Contact',
+        to: '/contact/'
+      }
+    ]
+  },
+  {
+    label: 'Join us',
+    to: '/join-us/'
+  }
+]
+
+export default navLinks
