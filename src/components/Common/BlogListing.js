40549bf83e5175a35b843f431ac71c5acc1bd8f0
Jennifer Spencer <jennifer.spencer@yld.io>
2019-04-30 12:09:19 +0100

    create BlogListing component using TitleAndMediaList

---
+++ src/components/Common/BlogListing.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Padding } from 'styled-components-spacing'
+import { format } from 'date-fns'
+
+import { Grid } from '../grid'
+import TitleAndMediaList from '../Common/TitleAndMediaList'
+
+const BlogListing = ({ title, description, posts }) => {
+  const mediaItems = posts.map(
+    ({ id, title, uniqueSlug, firstPublishedAt }) => {
+      return {
+        id,
+        title,
+        href: `https://medium.com/yld-engineering-blog/${uniqueSlug}`,
+        body: format(new Date(firstPublishedAt), 'MMMM DD[,] dddd')
+      }
+    }
+  )
+
+  return (
+    <Grid>
+      <Padding vertical={{ desktop: 5, smallPhone: 3.5 }}>
+        <TitleAndMediaList
+          title={title}
+          description={description}
+          mediaItems={mediaItems}
+          CTALink="https://medium.com/yld-engineering-blog"
+          CTAText="More articles"
+        />
+      </Padding>
+    </Grid>
+  )
+}
+
+export default BlogListing
