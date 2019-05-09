acc155d327bb78ee3fbaf275018f18f83f95d484
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-05-09 11:41:22 +0100

    Refactor getEvents

--- src/components/Homepage/events/getEvents.js
+++ src/components/Homepage/events/getEvents.js
@@ -1,27 +1,22 @@
 import { format, isAfter, endOfYesterday } from 'date-fns'
 
-const yesterday = new Date(endOfYesterday())
-const isAfterYesterday = date => isAfter(date, yesterday)
-
-const toDate = stringDate => new Date(stringDate)
-
-export const getHomepageMeetups = events =>
+export const getHomepageMeetups = (events = []) =>
   events
     .filter(
-      n => !n.node.homepageFeatured && isAfterYesterday(toDate(n.node.date))
+      n => !n.node.homepageFeatured && isAfter(n.node.date, endOfYesterday())
     )
-    .sort((a, b) => (toDate(a.node.date) <= toDate(b.node.date) ? -1 : 1))
+    .sort((a, b) => (a.node.date <= b.node.date ? -1 : 1))
     .slice(0, 5)
     .map(n => ({
       ...n.node,
-      date: format(toDate(n.node.date), 'MMMM DD[,] dddd')
+      date: format(n.node.date, 'MMMM DD[,] dddd')
     }))
 
-export const getHomepageConferences = events =>
+export const getHomepageConferences = (events = []) =>
   events
     .filter(n => n.node.homepageFeatured)
     .slice(0, 1)
     .map(n => ({
       ...n.node,
-      date: format(toDate(n.node.date), 'MMMM DD[,] dddd')
+      date: format(n.node.date, 'MMMM DD[,] dddd')
     }))
