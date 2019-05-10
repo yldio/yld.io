2fcec8a71a0198b49e486a61a461b9ef983995fd
Ollie Monk <ollie@olliemonk.com>
2019-05-10 19:58:50 +0800

    add CI steps

--- stories/storyshots.test.js
+++ stories/storyshots.test.js
@@ -1,19 +1,22 @@
 import initStoryshots from '@storybook/addon-storyshots'
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer'
 
-const beforeScreenshot = (page, { context: { kind, story }, url }) => {
+const { STATIC_BUILD } = process.env
+
+const beforeScreenshot = async page => {
   return new Promise(resolve =>
     setTimeout(() => {
       resolve()
-    }, 1200)
+    }, 600)
   )
 }
 
 initStoryshots({
   suite: 'Image storyshots',
   test: imageSnapshot({
-    storybookUrl: 'http://localhost:6006',
-    // storybookUrl: `file://${storybookPath}`,
+    storybookUrl: STATIC_BUILD
+      ? 'file://storybook-static'
+      : 'http://localhost:6006',
     beforeScreenshot
   })
 })
