9497f5b7d72564a6c271ea99c4a287af673786db
Ania <ania.makes@gmail.com>
2019-05-10 13:43:54 +0100

    added webpack-node-externals which allows back-end modules to be bundled without issues for the meetup lambda

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
+  externals: [nodeExternals()],
   mode: 'development',
   plugins: [new webpack.DefinePlugin({ 'global.GENTLY': false })]
 }
