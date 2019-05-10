19aee910772b79f4afc3b6021479aea16d8e8b17
Ollie Monk <ollie@olliemonk.com>
2019-05-10 20:17:46 +0800

    add STATIC_BUILD_PATH to ci config

--- stories/storyshots.test.js
+++ stories/storyshots.test.js
@@ -1,7 +1,7 @@
 import initStoryshots from '@storybook/addon-storyshots'
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer'
 
-const { STATIC_BUILD } = process.env
+const { STATIC_BUILD_PATH } = process.env
 
 const beforeScreenshot = async page => {
   return new Promise(resolve =>
@@ -14,8 +14,8 @@ const beforeScreenshot = async page => {
 initStoryshots({
   suite: 'Image storyshots',
   test: imageSnapshot({
-    storybookUrl: STATIC_BUILD
-      ? 'file://storybook-static'
+    storybookUrl: STATIC_BUILD_PATH
+      ? `file://${STATIC_BUILD_PATH}`
       : 'http://localhost:6006',
     beforeScreenshot
   })
