996df48fc67c5c88c61512f884b79222b9ebd2aa
Ollie Monk <ollie@olliemonk.com>
2019-05-17 12:35:50 +0800

    add babelrc false to babel-loader config in webpack

--- webpack.functions.js
+++ webpack.functions.js
@@ -3,6 +3,22 @@ const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   externals: [nodeExternals()],
-  mode: 'development',
-  plugins: [new webpack.DefinePlugin({ 'global.GENTLY': false })]
+  mode: 'production',
+  plugins: [new webpack.DefinePlugin({ 'global.GENTLY': false })],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false
+            }
+          }
+        ]
+      }
+    ]
+  }
 }
