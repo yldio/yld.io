515cf0efb4ac39ac5be6f842ab366767d6806c4d
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-05-20 16:33:24 +0100

    Update overflow-y to auto on SideNav

--- src/components/Header/SideNav/index.js
+++ src/components/Header/SideNav/index.js
@@ -16,7 +16,7 @@ const SideNavPanel = styled.nav`
   left: 0;
   top: 0;
   z-index: ${props => props.theme.zIndexes.header};
-  overflow-y: scroll;
+  overflow-y: auto;
   transform: translateX(100%);
   transition: transform ${props => props.theme.animations.fast} ease-in-out;
 
