023563f79721e37dc2520508f7692ee6606610a5
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-05-22 16:16:33 +0100

    Add first versionning of DesktopGDPR within the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Padding } from 'styled-components-spacing'
+import { Link } from 'gatsby'
+import breakpoint from 'styled-components-breakpoint'
 
 import { Grid, Row, Col } from '../../components/grid'
 import { SectionTitle } from '../../components/Typography'
@@ -8,7 +10,7 @@ import Locations from './Locations'
 import Office from './Office'
 import ExternalAnchor from '../Common/ExternalAnchor'
 
-import social from './links'
+import { social, gdpr } from './links'
 
 export const GreyFooter = styled.footer`
   background: #232323;
@@ -37,72 +39,81 @@ export const OfficeStyled = styled(Row)`
   overflow: hidden;
 `
 
-const CookiesAndPrivacyPolicies = styled.div`
+const DesktopGDPR = styled.div`
+  visibility: hidden;
+  position: absolute;
+  right: 0;
   color: white;
+
+  ${breakpoint('desktop')`
+    visibility: visible;
+  `}
 `
 
-const Footer = () => {
-  return (
-    <GreyFooter>
-      <Padding top={{ smallPhone: 3, tablet: 4 }}>
+const gdprLinksElement = gdpr.map(link => (
+  <Link key={link.text} to={link.to}>
+    {link.text}
+  </Link>
+))
+
+const Footer = () => (
+  <GreyFooter>
+    <Padding top={{ smallPhone: 3, tablet: 4 }}>
+      <Grid>
+        <Row>
+          <Col width={1}>
+            <Padding bottom={3}>
+              <SectionTitle reverse>Find us</SectionTitle>
+            </Padding>
+          </Col>
+        </Row>
+        <Padding bottom={{ smallPhone: 3.5, tablet: 5 }}>
+          <OfficeStyled>
+            <Locations>
+              {data =>
+                data.map(location => {
+                  const streetAddress = location.node.streetAddress.streetAddress.split(
+                    '\n'
+                  )
+
+                  return (
+                    <Office
+                      key={location.node.id}
+                      {...location.node}
+                      streetAddress={streetAddress}
+                    />
+                  )
+                })
+              }
+            </Locations>
+          </OfficeStyled>
+        </Padding>
+      </Grid>
+    </Padding>
+    <FollowUs>
+      <Padding top={3} bottom={3}>
         <Grid>
           <Row>
             <Col width={1}>
-              <Padding bottom={3}>
-                <SectionTitle reverse>Find us</SectionTitle>
-              </Padding>
+              <Social>
+                {social.map(s => (
+                  <li key={s.label}>
+                    <ExternalAnchor href={s.link}>
+                      <img src={s.img} alt={s.label} />
+                    </ExternalAnchor>
+                  </li>
+                ))}
+                <DesktopGDPR>{gdprLinksElement}</DesktopGDPR>
+              </Social>
             </Col>
           </Row>
-          <Padding bottom={{ smallPhone: 3.5, tablet: 5 }}>
-            <OfficeStyled>
-              <Locations>
-                {data =>
-                  data.map(location => {
-                    const streetAddress = location.node.streetAddress.streetAddress.split(
-                      '\n'
-                    )
-
-                    return (
-                      <Office
-                        key={location.node.id}
-                        {...location.node}
-                        streetAddress={streetAddress}
-                      />
-                    )
-                  })
-                }
-              </Locations>
-            </OfficeStyled>
-          </Padding>
         </Grid>
       </Padding>
-      <FollowUs>
-        <Padding top={3} bottom={3}>
-          <Grid>
-            <Row>
-              <Col width={1}>
-                <Social>
-                  {social.map(s => (
-                    <li key={s.label}>
-                      <ExternalAnchor href={s.link}>
-                        <img src={s.img} alt={s.label} />
-                      </ExternalAnchor>
-                    </li>
-                  ))}
-                  <CookiesAndPrivacyPolicies>
-                    Desktop Cookies and privacy policies placeholder
-                  </CookiesAndPrivacyPolicies>
-                </Social>
-              </Col>
-            </Row>
-          </Grid>
-        </Padding>
-      </FollowUs>
-      <CookiesAndPrivacyPolicies>
+    </FollowUs>
+    {/* <DesktopGDPR>
         Mobile and tablets Cookies and privacy policies placeholder
-      </CookiesAndPrivacyPolicies>
-    </GreyFooter>
-  )
-}
+      </DesktopGDPR> */}
+  </GreyFooter>
+)
 
 export default Footer
