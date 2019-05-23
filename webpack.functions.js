7ab39cb32b8b7b547144d31370796f3dfba0b51d
Ollie Monk <omonk@users.noreply.github.com>
2019-05-24 00:50:28 +0800

    470 connect external widgets (#203)
    * update readme about automated deployments
    
    * remove functions/, add build cmd to netlify config
    
    * change scripts, update docs
    
    * comment update
    
    * update scripts docs
    
    * add babelrc false to babel-loader config in webpack
    
    * update netlify-lambda with new --babelrc flag
    
    * add lever lambda
    
    * add logging for netlify logs
    
    * log deploy_url
    
    * update lever lambda
    
    * log the event and context vars
    
    * get host from event headers
    
    * all the logs
    
    * add NODE_ENV
    
    * remove node env from content
    
    * remove node env from content
    
    * double quote?
    
    * add LAMBDA_ENV to toml, use in lever lambda
    
    * update readme
    
    * revert all changes to meetup lambda
    
    * move netlify-lambda to devDeps
    
    * move netlify-lambda to devDeps
    
    * Revert "move netlify-lambda to devDeps"
    
    This reverts commit c7a5a5a91872e4fe130f0241137415d1164b7207.
    
    * readme and documentation updates
    
    * add default to NODE_ENV

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,8 +1,10 @@
 const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
 
+// I don't think NODE_ENV is even set on the Netlify function environment...
+const { NODE_ENV = 'production' } = process.env
 module.exports = {
   externals: [nodeExternals()],
-  mode: 'development',
+  mode: NODE_ENV,
   plugins: [new webpack.DefinePlugin({ 'global.GENTLY': false })]
 }
