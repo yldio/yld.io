40fb29c65194a9bddce9794fcfd11f14dd7ad317
Ania <ania.makes@gmail.com>
2019-05-23 15:24:25 +0100

    Attempt at component and a story

---
+++ src/components/OpenSource/WhyOpenSource.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+import SubtitleWithBody from '../Common/SubtitleWithBody'
+import styled from 'styled-components'
+
+// const StyledSubtitleWithBody = styled(SubtitleWithBody)`
+//     padding-top: ${props => props.theme.spacing['1']};
+//     padding-bottom: ${props => props.theme.spacing['1']};
+//     margin-top: ${props => props.theme.spacing['1']};
+//     margin-bottom: ${props => props.theme.spacing['2']};
+// `
+
+const WhyOpenSource = ({ subtitle, body, themeVariation = 'white' }) => (
+    <Fragment>
+        <SubtitleWithBody subtitle body themeVariation />
+    </Fragment>
+)
+
+export default WhyOpenSource
