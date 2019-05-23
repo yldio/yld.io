e0fa44e5a5216f6f9597bb53088cbd16c21aee3b
Ania <ania.makes@gmail.com>
2019-05-23 15:24:25 +0100

    Attempt at component and a story
    minor changes
    
    added image to the component
    
    added margins to img
    
    updated storybook snapshots

---
+++ src/components/OpenSource/WhyOpenSource.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react'
+import SubtitleWithBody from '../Common/SubtitleWithBody'
+import styled from 'styled-components'
+
+const StyledSubtitleWithBody = styled(SubtitleWithBody)`
+  padding-top: ${props => props.theme.spacing['1']};
+  padding-bottom: ${props => props.theme.spacing['1']};
+  margin-top: ${props => props.theme.spacing['1']};
+  margin-bottom: ${props => props.theme.spacing['2']};
+`
+
+const StyledImage = styled.img`
+  padding-top: ${props => props.theme.spacing['2']};
+`
+
+const WhyOpenSource = ({ icon, subtitle, body, themeVariation = 'white' }) => (
+  <Fragment>
+    <StyledImage src={icon} />
+    <StyledSubtitleWithBody
+      subtitle={subtitle}
+      body={body}
+      themeVariation={themeVariation}
+    />
+  </Fragment>
+)
+
+export default WhyOpenSource
---
+++ stories/LogoSubtitleBody.stories.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { storiesOf, addDecorator } from '@storybook/react'
+import Theme from './theme'
+import { Grid, Col, Row } from '../src/components/grid'
+import WhyOpenSource from '../src/components/OpenSource/WhyOpenSource'
+
+import icon from '../storybook-static/static/media/community_support.svg'
+
+addDecorator(Theme)
+
+const title = 'Community support'
+const body =
+  'Open source technologies are supported by knowledgeable and resourceful communities. Their collective contributions result in more robust, innovative and faster solutions.'
+
+storiesOf('Logo, Subtitle & Body', module).add('on white Background', () => {
+  return (
+    <Grid>
+      <Row>
+        <Col width={[1, 1, 1, 1, 0.5]}>
+          <WhyOpenSource
+            icon={icon}
+            subtitle={title}
+            body={body}
+            themeVariation="white"
+          />
+        </Col>
+      </Row>
+    </Grid>
+  )
+})
