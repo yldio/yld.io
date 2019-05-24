6d7504536564a7b4056fb41a60b9ac4f07eb0fc9
Ania <ania.makes@gmail.com>
2019-05-24 09:36:41 +0100

    minor changes

--- stories/LogoSubtitleBody.stories.js
+++ stories/LogoSubtitleBody.stories.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { storiesOf, addDecorator } from '@storybook/react'
 import Theme from './theme'
-import BlueBackground from '../src/components/Common/BlueBackground'
-import GreyBackground from '../src/components/Common/GreyBackground'
 import { Grid, Col, Row } from '../src/components/grid'
 import WhyOpenSource from '../src/components/OpenSource/WhyOpenSource'
 
@@ -10,21 +8,16 @@ addDecorator(Theme)
 
 const title = 'Community support'
 const body =
-    'Open source technologies are supported by knowledgeable and resourceful communities. Their collective contributions result in more robust, innovative and faster solutions.'
+  'Open source technologies are supported by knowledgeable and resourceful communities. Their collective contributions result in more robust, innovative and faster solutions.'
 
-storiesOf('Logo, Subtitle & Body', module)
-    .add('on white Background', () => {
-        return (
-            <Grid>
-                <Row>
-                    <Col width={[1, 1, 1, 1, 0.5]}>
-                        <WhyOpenSource
-                            subtitle={title}
-                            body={body}
-                            themeVariation="white"
-                        />
-                    </Col>
-                </Row>
-            </Grid>
-        )
-    })
+storiesOf('Logo, Subtitle & Body', module).add('on white Background', () => {
+  return (
+    <Grid>
+      <Row>
+        <Col width={[1, 1, 1, 1, 0.5]}>
+          <WhyOpenSource subtitle={title} body={body} themeVariation="white" />
+        </Col>
+      </Row>
+    </Grid>
+  )
+})
