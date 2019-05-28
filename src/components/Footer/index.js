d15838f2c5ec5bff59b708608f576cff039b7d5e
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-05-28 11:27:55 +0100

    Move OfficeListing to their own file

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,16 +4,11 @@ import { Padding } from 'styled-components-spacing'
 
 import { Grid, Row, Col } from '../../components/grid'
 import { SectionTitle } from '../../components/Typography'
+import OfficeListing from './office-listing'
 import FooterLinks from './footer-links'
-import Locations from './Locations'
-import Office from './Office'
 
 export const GreyFooter = styled.footer`
   background: #232323;
-`
-
-export const OfficeStyled = styled(Row)`
-  overflow: hidden;
 `
 
 const Footer = () => (
@@ -27,27 +22,7 @@ const Footer = () => (
             </Padding>
           </Col>
         </Row>
-        <Padding bottom={{ smallPhone: 3.5, tablet: 5 }}>
-          <OfficeStyled>
-            <Locations>
-              {data =>
-                data.map(location => {
-                  const streetAddress = location.node.streetAddress.streetAddress.split(
-                    '\n'
-                  )
-
-                  return (
-                    <Office
-                      key={location.node.id}
-                      {...location.node}
-                      streetAddress={streetAddress}
-                    />
-                  )
-                })
-              }
-            </Locations>
-          </OfficeStyled>
-        </Padding>
+        <OfficeListing />
       </Grid>
     </Padding>
     <FooterLinks />
