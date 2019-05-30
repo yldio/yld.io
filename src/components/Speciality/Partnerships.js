ba0414882342732eaeb1675f8359ebe5abba3ca9
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-05-30 09:56:52 +0100

    Add iterated PartnerCol component within Partnerships

--- src/components/Speciality/Partnerships.js
+++ src/components/Speciality/Partnerships.js
@@ -3,11 +3,38 @@ import React from 'react'
 import BlueBackground from '../Common/BlueBackground'
 import { Grid, Row, Col } from '../grid'
 import { SectionTitle } from '../Typography'
+import SubtitleWithBody from '../Common/SubtitleWithBody'
 
 // will come from Contentful
 const data = {
-  title: 'Our Technology partnerships'
+  title: 'Our Technology partnerships',
+  partners: [
+    {
+      name: 'Node foundation',
+      type: 'Silver member',
+      description:
+        'YLD was one the first members of the Node.js Foundation, whose purpose is to support the ongoing growth and evolution of the Node.js platform, while maintaining a collaborative environment that benefits all.'
+    },
+    {
+      name: 'Cloud native',
+      type: 'Silver member',
+      description:
+        'We are members of the Cloud Native Computing Foundation, whose purpose is to foster the community by sustaining and integrating open-source technologies such as Kubernetes and orchestrating containers as part of a microservices architecture.'
+    },
+    {
+      name: 'Docker',
+      type: 'Silver member',
+      description:
+        'YLD was one the first members of the Node.js Foundation, whose purpose is to support the ongoing growth and evolution of the Node.js platform, while maintaining a collaborative environment that benefits all.'
+    }
+  ]
 }
+
+const PartnerCol = ({ name, type, description }) => (
+  <Col key={name} width={1 / 3}>
+    <SubtitleWithBody subtitle={type} body={description} />
+  </Col>
+)
 
 const PartnershipsSection = () => (
   <BlueBackground>
@@ -17,6 +44,7 @@ const PartnershipsSection = () => (
           <SectionTitle reverse>{data.title}</SectionTitle>
         </Col>
       </Row>
+      <Row>{data.partners.map(partner => PartnerCol(partner))}</Row>
     </Grid>
   </BlueBackground>
 )
