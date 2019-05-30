934c10920c6dae0a5116777b1115d5a6feb3768e
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-05-30 15:44:37 +0100

    Our-technology-partnerships-section (#221)
    * Add PartnershipsSection with a SectionTitle
    
    * Add iterated PartnerCol component within Partnerships
    
    * Move Partnerships to OpenSource folder
    
    * Add Image component within Partnerships and rename keys to match Contentful
    
    * Resolve openSource page graphQL query and make page accessible via dropdown menu
    
    * Add Partnerships section to OpenSource page
    
    * Redefine PartnershipsSection so it uses data from contentful
    
    * Retrieve image file urls from contentful through graphQL
    
    * Add first versionning of styling
    
    * Add StyledImageContainer to manage the image resizing since logos have different sizes
    
    * Add responsive design for section
    
    * Update snapshots
    
    * Update responsive design of StyledSectionTitle
    
    * Add validation before mapping on partners
    
    * Remove typo

---
+++ src/components/OpenSource/Partnerships.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components'
+import remcalc from 'remcalc'
+
+import BlueBackground from '../Common/BlueBackground'
+import { Grid, Row, Col } from '../grid'
+import { SectionTitle } from '../Typography'
+import Image from '../Common/Image'
+import SubtitleWithBody from '../Common/SubtitleWithBody'
+
+const StyledSectionTitle = styled(Col)`
+  padding: ${remcalc(83)} 0;
+`
+
+const StyledPartnersRow = styled(Row)`
+  padding-bottom: ${remcalc(60)};
+`
+
+const StyledImage = styled(Image)`
+  max-height: 100%;
+  max-width: 60%;
+`
+
+const StyledImageContainer = styled.div`
+  height: ${remcalc(108)};
+`
+
+const PartnerCol = ({ name, logoDarkTheme, membershipLevel, description }) => (
+  <Col key={name} width={[1, 1, 1, 1, 1 / 2, 1 / 2, 1 / 3]}>
+    <StyledImageContainer>
+      <StyledImage
+        image={logoDarkTheme}
+        alt={`Image of ${logoDarkTheme.title}`}
+      />
+    </StyledImageContainer>
+    <SubtitleWithBody
+      subtitle={membershipLevel}
+      body={description}
+      themeVariation="dark"
+    />
+  </Col>
+)
+
+const PartnershipsSection = ({ title, partners }) => (
+  <BlueBackground>
+    <Grid>
+      <Row>
+        <StyledSectionTitle width={[1, 1, 1, 1, 1 / 2, 1 / 2, 1 / 2]}>
+          <SectionTitle reverse>{title}</SectionTitle>
+        </StyledSectionTitle>
+      </Row>
+      <StyledPartnersRow>
+        {partners &&
+          partners.length &&
+          partners.map(partner => PartnerCol(partner))}
+      </StyledPartnersRow>
+    </Grid>
+  </BlueBackground>
+)
+
+export default PartnershipsSection
