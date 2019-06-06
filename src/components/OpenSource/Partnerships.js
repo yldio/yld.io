4f938b41b98a64679756c8825321bd0cc9dbcd42
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-06-06 11:57:57 +0100

    Os partnerships section logos fix (#247)
    * destructure theme from props in styling for both talks and partnerships sections
    
    * Add StyledImageContainer around image so it resizes as expected

--- src/components/OpenSource/Partnerships.js
+++ src/components/OpenSource/Partnerships.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import breakpoint from 'styled-components-breakpoint'
+import remcalc from 'remcalc'
 
 import BlueBackground from '../Common/BlueBackground'
 import { Grid, Row, Col } from '../grid'
@@ -9,26 +10,43 @@ import Image from '../Common/Image'
 import SubtitleWithBody from '../Common/SubtitleWithBody'
 
 const StyledGrid = styled(Grid)`
-  padding-top: ${props => props.theme.space[4]};
-  padding-bottom: ${props => props.theme.space[5]};
+  padding-top: ${({ theme }) => theme.space[4]};
+  padding-bottom: ${({ theme }) => theme.space[5]};
 
   ${breakpoint('smallTablet')`
-    padding-top: ${props => props.theme.space[6]};
-    padding-bottom: ${props => props.theme.space[7]};
+    padding-top: ${({ theme }) => theme.space[6]};
+    padding-bottom: ${({ theme }) => theme.space[7]};
   `}
 `
 
 const StyledRow = styled(Row)`
-  padding-bottom: ${props => props.theme.space[6]};
+  padding-bottom: ${({ theme }) => theme.space[6]};
 `
 
 const StyledSubtitleWithBodyContainer = styled.div`
-  padding-top: ${props => props.theme.spacing[1]};
+  padding-top: ${({ theme }) => theme.spacing[1]};
+`
+
+const StyledImageContainer = styled.div`
+  width: ${remcalc(200)};
+  height: ${remcalc(90)};
+
+  ${breakpoint('tablet')`
+    width: ${remcalc(165)};
+    height: ${remcalc(75)};
+  `}
+
+  ${breakpoint('desktop')`
+    width: ${remcalc(239)};
+    height: ${remcalc(108)};
+  `}
 `
 
 const PartnerCol = ({ name, logoDarkTheme, membershipLevel, description }) => (
   <Col key={name} width={[1, 1, 1, 1, 1 / 2, 1 / 2, 1 / 3]}>
-    <Image image={logoDarkTheme} alt={`Image of ${logoDarkTheme.title}`} />
+    <StyledImageContainer>
+      <Image image={logoDarkTheme} alt={`Image of ${logoDarkTheme.title}`} />
+    </StyledImageContainer>
     <StyledSubtitleWithBodyContainer>
       <SubtitleWithBody
         subtitle={membershipLevel}
