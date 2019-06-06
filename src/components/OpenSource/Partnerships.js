eca4cba56500bf44d8e017294d01f8024a9bb599
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-06-06 10:27:50 +0100

    Add StyledImageContainer around image so it resizes as expected

--- src/components/OpenSource/Partnerships.js
+++ src/components/OpenSource/Partnerships.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import breakpoint from 'styled-components-breakpoint'
+import remcalc from 'remcalc'
 
 import BlueBackground from '../Common/BlueBackground'
 import { Grid, Row, Col } from '../grid'
@@ -26,9 +27,26 @@ const StyledSubtitleWithBodyContainer = styled.div`
   padding-top: ${({ theme }) => theme.spacing[1]};
 `
 
+const StyledImageContainer = styled.div`
+  width: ${remcalc(200)};
+  height: ${remcalc(90)};
+
+  ${breakpoint('tablet')`
+    width: ${remcalc(165)};
+    height: ${remcalc(75)};
+  `}
+
+  ${breakpoint('desktop')`
+    width: ${remcalc(239)};
+    height: ${remcalc(108)};
+  `}
+`
+
 const PartnerCol = ({ name, logoDarkTheme, membershipLevel, description }) => (
   <Col key={name} width={[1, 1, 1, 1, 1 / 2, 1 / 2, 1 / 3]}>
-    <Image image={logoDarkTheme} alt={`Image of ${logoDarkTheme.title}`} />
+    <StyledImageContainer>
+      <Image image={logoDarkTheme} alt={`Image of ${logoDarkTheme.title}`} />
+    </StyledImageContainer>
     <StyledSubtitleWithBodyContainer>
       <SubtitleWithBody
         subtitle={membershipLevel}
