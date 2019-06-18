59cbf79cdb87462548e17cf8b59d9d84909dd6a8
Ania <31079643+AniaMakes@users.noreply.github.com>
2019-06-18 04:29:26 +0100

    add scroll to functionality for join-us page (#265)
    * Add scroll to functionality
    
    added fake link styling, added some modules so that the build would run no prob
    
    moved FakeLink to StyledLink
    
    * removed stray comment
    
    * remove commnet

--- src/components/JoinUs/ViewOpenPositions.js
+++ src/components/JoinUs/ViewOpenPositions.js
@@ -3,35 +3,50 @@ import remcalc from 'remcalc'
 import styled from 'styled-components'
 import { Padding } from 'styled-components-spacing'
 import { Grid, Row, Col } from '../grid'
-import StyledLink from '../Common/StyledLink'
 import { SectionTitle, BodyPrimary } from '../Typography'
+import FakeLink from '../Common/StyledLink'
 
 const FixedWidthBodyPrimary = styled(BodyPrimary)`
   max-width: ${remcalc(525)};
 `
-const ViewPositions = ({ text, description }) => (
-  <Grid>
-    <Padding
-      top={{ smallPhone: 4, smallTablet: 3, tablet: 4 }}
-      bottom={{ smallPhone: 3.5, desktop: 5 }}
-    >
-      <Row>
-        <Col width={[1, 1, 1, 1, 8 / 12]}>
-          <Padding bottom={{ smallPhone: 2, tablet: 3 }}>
-            <SectionTitle as="h1">{text}</SectionTitle>
-            {description && (
-              <Padding top={1}>
-                <FixedWidthBodyPrimary muted>
-                  {description}
-                </FixedWidthBodyPrimary>
+
+/**
+ * This component needs to be a class to avoid
+ * React shouting at us because functional components
+ * are not allowed to have refs. The ref here is used
+ * by the scrollTo module we are using within `<JoinUs/>`
+ *
+ */
+class ViewPositions extends React.Component {
+  render() {
+    const { text, description, scrollToVacancies } = this.props
+    return (
+      <Grid>
+        <Padding
+          top={{ smallPhone: 4, smallTablet: 3, tablet: 4 }}
+          bottom={{ smallPhone: 3.5, desktop: 5 }}
+        >
+          <Row>
+            <Col width={[1, 1, 1, 1, 8 / 12]}>
+              <Padding bottom={{ smallPhone: 2, tablet: 3 }}>
+                <SectionTitle as="h1">{text}</SectionTitle>
+                {description && (
+                  <Padding top={1}>
+                    <FixedWidthBodyPrimary muted>
+                      {description}
+                    </FixedWidthBodyPrimary>
+                  </Padding>
+                )}
               </Padding>
-            )}
-          </Padding>
-          <StyledLink href="#open-positions">All job opportunities</StyledLink>
-        </Col>
-      </Row>
-    </Padding>
-  </Grid>
-)
+              <FakeLink onClick={() => scrollToVacancies()}>
+                All job opportunities
+              </FakeLink>
+            </Col>
+          </Row>
+        </Padding>
+      </Grid>
+    )
+  }
+}
 
 export default ViewPositions
