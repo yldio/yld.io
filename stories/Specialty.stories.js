e7be3d985885354dd660d9c70082320641cc4131
Ania <ania.makes@gmail.com>
2019-06-19 16:55:44 +0100

    end of day / created speciality story

---
+++ stories/Specialty.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { storiesOf, addDecorator } from '@storybook/react'
+import Theme from './theme'
+
+import Speciality from '../src/templates/speciality'
+
+import Books from '../src/components/Speciality/Books'
+import Community from '../src/components/Speciality/Community'
+import Events from '../src/components/Speciality/Events'
+import Intro from '../src/components/Speciality/Intro'
+import Projects from '../src/components/Speciality/Projects'
+import Talks from '../src/components/Speciality/Talks'
+import Training from '../src/components/Speciality/Training'
+import Tutorials from '../src/components/Speciality/Tutorials'
