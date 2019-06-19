0daaff0e3e3353010a270ef4916fec1635bb3200
Ollie Monk <ollie@olliemonk.com>
2019-06-19 10:42:50 +0800

    add timeouts to integration tests

--- tests/e2e/topNav-dropdown-test.js
+++ tests/e2e/topNav-dropdown-test.js
@@ -45,19 +45,17 @@ test('An outerAnchorItem redirects to a page', async t => {
 
 test('opens and close a dropdown', async t => {
   const services = await Selector('li').withText('Services')
-  await t.expect(services.exists).ok()
+  await t.expect(services.exists).ok({ timeout: 5000 })
   await t.click(services)
 
   const openDropdown = Selector('li')
     .withText('Services')
     .withAttribute('aria-expanded', 'true')
-  await t.expect(openDropdown.exists).ok()
+  await t.expect(openDropdown.exists).ok({ timeout: 5000 })
 
   const elsewhere = Selector('section').nth(0)
   await t.click(elsewhere)
 
-  const closedDropdown = Selector('li')
-    .withText('Services')
-    .withAttribute('aria-expanded', 'false')
-  await t.expect(closedDropdown.exists).ok()
+  const closedDropdown = services.withAttribute('aria-expanded', 'false').nth(0)
+  await t.expect(closedDropdown.exists).ok({ timeout: 5000 })
 })
