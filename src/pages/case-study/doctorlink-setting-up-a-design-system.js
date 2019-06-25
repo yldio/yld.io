7c98a095502b2e551043dce22eeccb147f3e918b
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-06-25 15:52:55 +0100

    Add Blue and Grey backgrounds where needed and pass a columns arg to renderBlock

--- src/pages/case-study/doctorlink-setting-up-a-design-system.js
+++ src/pages/case-study/doctorlink-setting-up-a-design-system.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { graphql } from 'gatsby'
 import ReactMarkdown from 'react-markdown'
 import styled from 'styled-components'
