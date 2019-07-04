89803ac964b59520f7fa11a23373e0f715db68c5
Ollie Monk <ollie@olliemonk.com>
2019-07-04 11:03:04 +0800

    init central working
    remove a few unused packages
    
    update
    
    padding update
    
    update padding
    
    update
    
    update yarnlock
    
    re-add raf
    
    Brown -> Black, remove Fragment

---
+++ src/components/Common/BlackBackground.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export default styled.section`
+  background-color: ${({ theme }) => theme.colors.black};
+`
---
+++ src/components/Common/TanBackground.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export default styled.section`
+  background: ${props => props.theme.colors.tanBg};
+`
