ed7a2498c3d9af8746cf1600f4b6a1e533b272db
Semra Shukrieva <semra.shukrieva@yld.io>
2019-07-04 16:40:36 +0100

    remove Padding component and add styled components

--- src/components/Common/OpenPositions.js
+++ src/components/Common/OpenPositions.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Padding } from 'styled-components-spacing'
+import styled from 'styled-components'
 
-import { Col, Row } from '../grid'
-import JobsByLocation from '../JobsByLocation'
+import { Col, Row, Grid } from '../grid'
+import JobsByLocation from './JobsByLocation'
 import { SectionTitle, Subtitle } from '../Typography'
 import JobLink from './JobLink'
+import GreyBackground from './GreyBackground'
 
 export const OpenPositionsWithRef = React.forwardRef((props, ref) => (
   <div ref={ref}>
@@ -12,44 +14,51 @@ export const OpenPositionsWithRef = React.forwardRef((props, ref) => (
   </div>
 ))
 
+const StyledOpenPositions = styled(GreyBackground)`
+  padding-top: ${({ theme }) => theme.space[6]};
+  padding-bottom: ${({ theme }) => theme.space[6]};
+`
+
 const OpenPositions = ({ data: { title } }) => (
-  <Padding top={{ smallPhone: 3, tablet: 4 }}>
-    <SectionTitle>{title}</SectionTitle>
-    <JobsByLocation>
-      {jobs =>
-        jobs.map(({ location, jobs }, idx) => (
-          <Padding top={3} key={idx}>
-            <Subtitle>{location}</Subtitle>
-            <Padding top={2}>
-              <Row as="ul">
-                {jobs.map((job, idx) => {
-                  const {
-                    text,
-                    hostedUrl,
-                    categories: { commitment }
-                  } = job.node
-                  return (
-                    <Col
-                      key={`job-${location}-${idx}`}
-                      as="li"
-                      width={[1, 1, 1, 1, 4 / 12, 3 / 12]}
-                    >
-                      <JobLink
-                        location={location}
-                        position={text}
-                        hostedUrl={hostedUrl}
-                        contractType={commitment}
-                      />
-                    </Col>
-                  )
-                })}
-              </Row>
+  <StyledOpenPositions>
+    <Grid>
+      <SectionTitle>{title}</SectionTitle>
+      <JobsByLocation>
+        {jobs =>
+          jobs.map(({ location, jobs }, idx) => (
+            <Padding top={3} key={idx}>
+              <Subtitle>{location}</Subtitle>
+              <Padding top={2}>
+                <Row as="ul">
+                  {jobs.map((job, idx) => {
+                    const {
+                      text,
+                      hostedUrl,
+                      categories: { commitment }
+                    } = job.node
+                    return (
+                      <Col
+                        key={`job-${location}-${idx}`}
+                        as="li"
+                        width={[1, 1, 1, 1, 4 / 12, 3 / 12]}
+                      >
+                        <JobLink
+                          location={location}
+                          position={text}
+                          hostedUrl={hostedUrl}
+                          contractType={commitment}
+                        />
+                      </Col>
+                    )
+                  })}
+                </Row>
+              </Padding>
             </Padding>
-          </Padding>
-        ))
-      }
-    </JobsByLocation>
-  </Padding>
+          ))
+        }
+      </JobsByLocation>
+    </Grid>
+  </StyledOpenPositions>
 )
 
 export default OpenPositions
--- src/components/Homepage/jobs.js
+++ src/components/Homepage/jobs.js
@@ -1,27 +1,32 @@
-import React from 'react'
-import { Row, Col, Grid } from '../grid'
-import { Padding } from 'styled-components-spacing'
+import React, { Fragment } from 'react'
+import styled from 'styled-components'
 
+import { Row, Col, Grid } from '../grid'
 import StyledLink from '../Common/StyledLink'
 import OpenPositions from '../Common/OpenPositions'
 
+//{ smallPhone: 3, smallTablet: 4, desktop: 3.5 }
+const StyledGrid = styled(Grid)`
+  padding-bottom: ${({ theme }) => theme.space[6]};
+`
+
 const Jobs = () => (
-  <Grid pb={3}>
+  <Fragment>
     <OpenPositions
       data={{
         title: 'Join our team'
       }}
     />
-    <Row>
-      <Col width={[1]}>
-        <Padding top={{ smallPhone: 3, smallTablet: 4, desktop: 3.5 }}>
+    <StyledGrid>
+      <Row>
+        <Col width={[1]}>
           <StyledLink to="/join-us" title="View all openings">
             View all openings
           </StyledLink>
-        </Padding>
-      </Col>
-    </Row>
-  </Grid>
+        </Col>
+      </Row>
+    </StyledGrid>
+  </Fragment>
 )
 
 export default Jobs
