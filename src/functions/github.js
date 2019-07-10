b74772ae04f3bbe700814dda6f96e04a3c2c08cd
Ollie Monk <omonk@users.noreply.github.com>
2019-07-10 11:51:52 +0800

    add auth to lambdas (#288)
    * add auth to lambdas
    
    * wrap lambdas in Auth
    
    * use basic auth instead
    
    * update yarn lock

--- src/functions/github.js
+++ src/functions/github.js
@@ -12,6 +12,7 @@ const {
 } = require('@yldio/oss-stats')
 const { createClient } = require('contentful-management')
 
+const Auth = require('./utils/auth')
 const Meta = require('./oss/meta')
 const Repos = require('./oss/repos')
 
@@ -19,47 +20,50 @@ const { CONTENTFUL_SPACE, CMS_CRUD, GITHUB_TOKEN } = process.env
 
 const org = 'yldio'
 
-exports.handler = async () => {
-  if ((!CONTENTFUL_SPACE, !CMS_CRUD, !GITHUB_TOKEN)) {
-    throw new Error(`Missing env variables, check set up`)
-  }
+exports.handler = async evt =>
+  Auth(evt, async () => {
+    if ((!CONTENTFUL_SPACE, !CMS_CRUD, !GITHUB_TOKEN)) {
+      throw new Error(`Missing env variables, check set up`)
+    }
 
-  // Get github data
-  const {
-    repos,
-    repoCount: openSourceMetaReposCount,
-    pullRequestCount: openSourceMetaPullRequestsCount
-  } = await getData({
-    org,
-    token: GITHUB_TOKEN
-  })
-    .then(normalise)
-    .then(summariseContributions)
-
-  // Get contentful data
-  const client = createClient({
-    accessToken: CMS_CRUD
-  })
-
-  const space = await client.getSpace(CONTENTFUL_SPACE)
-  const environment = await space.getEnvironment('master')
-
-  const [meta, updatedRepos] = await Promise.all([
-    Meta(environment, {
-      openSourceMetaPullRequestsCount,
-      openSourceMetaReposCount
-    }),
-    Repos(environment, { repos })
-  ]).catch(err => {
-    throw new Error(err)
-  })
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      meta: { ...meta },
-      updatedRepos:
-        updatedRepos && updatedRepos.length ? updatedRepos : 'No repos updated'
+    // Get github data
+    const {
+      repos,
+      repoCount: openSourceMetaReposCount,
+      pullRequestCount: openSourceMetaPullRequestsCount
+    } = await getData({
+      org,
+      token: GITHUB_TOKEN
     })
-  }
-}
+      .then(normalise)
+      .then(summariseContributions)
+
+    // Get contentful data
+    const client = createClient({
+      accessToken: CMS_CRUD
+    })
+
+    const space = await client.getSpace(CONTENTFUL_SPACE)
+    const environment = await space.getEnvironment('master')
+
+    const [meta, updatedRepos] = await Promise.all([
+      Meta(environment, {
+        openSourceMetaPullRequestsCount,
+        openSourceMetaReposCount
+      }),
+      Repos(environment, { repos })
+    ]).catch(err => {
+      throw new Error(err)
+    })
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        meta: { ...meta },
+        updatedRepos:
+          updatedRepos && updatedRepos.length
+            ? updatedRepos
+            : 'No repos updated'
+      })
+    }
+  })
---
+++ src/functions/utils/auth.js
@@ -0,0 +1,42 @@
+const { LAMBDA_AUTH_TOKEN } = process.env
+
+const parseBasicToken = req => {
+  const auth = req.headers ? req.headers.authorization || null : null
+
+  if (!auth) {
+    return null
+  }
+
+  const parts = auth.split(' ')
+
+  // Malformed header.
+  if (parts.length < 2) {
+    return null
+  }
+
+  const schema = parts.shift().toLowerCase()
+  const token = parts.join(' ')
+
+  if (schema !== 'basic') {
+    return null
+  }
+
+  return token
+}
+
+const auth = (event, cb) => {
+  const token = parseBasicToken(event)
+
+  if (!token) {
+    throw new Error('Unable to read auth token')
+  }
+
+  return Object.is(token, LAMBDA_AUTH_TOKEN)
+    ? cb()
+    : {
+        statusCode: 401,
+        body: 'Not authenticated'
+      }
+}
+
+module.exports = auth
