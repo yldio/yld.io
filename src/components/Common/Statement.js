209f04bb5332019a70216e5e5a269c682208cef2
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-07-17 17:22:19 +0100

    Update Statement so we can render bold texts

--- src/components/Common/Statement.js
+++ src/components/Common/Statement.js
@@ -8,12 +8,12 @@ import { Row, Col, Grid } from '../grid'
 import { DisplayTitle } from '../Typography'
 
 const PaddedGrid = styled(Grid)`
-  padding-top: ${props => props.theme.space[4]};
-  padding-bottom: ${props => props.theme.space[4]};
+  padding-top: ${({ theme }) => theme.space[4]};
+  padding-bottom: ${({ theme }) => theme.space[4]};
 
   ${breakpoint('tablet')`
-    padding-top: ${props => props.theme.space[6]};
-    padding-bottom: ${props => props.theme.space[6]};
+    padding-top: ${({ theme }) => theme.space[6]};
+    padding-bottom: ${({ theme }) => theme.space[6]};
   `}
 `
 
@@ -26,35 +26,48 @@ const Link = styled(StyledLink)`
   display: initial;
 `
 
-const Statement = ({ richText, children, as = 'h2' }) => (
-  <GreyBackground>
-    <PaddedGrid>
-      <Row>
-        <Col width={[1, 1, 1, 10 / 12, 10 / 12, 9 / 12]}>
-          <DisplayTitle as={as} textLight>
-            {children}
-            {richText &&
-              richText.map(content => {
-                if (content.nodeType === 'text') return content.value
+const StyledStrong = styled.span`
+  color: ${({ theme }) => theme.colors.text};
+  font-weight: 500;
+`
 
-                if (content.nodeType === 'hyperlink') {
-                  return (
-                    <Link
-                      key={content.data.uri}
-                      noafter="true"
-                      to={`${content.data.uri}`}
-                    >
-                      {content.content[0].value}
-                    </Link>
-                  )
-                }
-                return ''
-              })}
-          </DisplayTitle>
-        </Col>
-      </Row>
-    </PaddedGrid>
-  </GreyBackground>
-)
+const makeStrong = text => <StyledStrong>{text}</StyledStrong>
+
+const Statement = ({ richText, children, as = 'h2' }) => {
+  const childrenArray = children
+    .split('__')
+    .map((child, i) => (i % 2 === 1 ? makeStrong(child) : child))
+
+  return (
+    <GreyBackground>
+      <PaddedGrid>
+        <Row>
+          <Col width={[1, 1, 1, 10 / 12, 10 / 12, 9 / 12]}>
+            <DisplayTitle as={as} textLight>
+              {childrenArray}
+              {richText &&
+                richText.map(content => {
+                  if (content.nodeType === 'text') return content.value
+
+                  if (content.nodeType === 'hyperlink') {
+                    return (
+                      <Link
+                        key={content.data.uri}
+                        noafter="true"
+                        to={`${content.data.uri}`}
+                      >
+                        {content.content[0].value}
+                      </Link>
+                    )
+                  }
+                  return ''
+                })}
+            </DisplayTitle>
+          </Col>
+        </Row>
+      </PaddedGrid>
+    </GreyBackground>
+  )
+}
 
 export default Statement
