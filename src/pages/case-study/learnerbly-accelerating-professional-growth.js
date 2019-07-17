ea1077a78645dc20a39ba0fa9a91c2e4418f9c4c
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-07-17 10:29:13 +0100

    Add paddings for BlockRow 7

--- src/pages/case-study/learnerbly-accelerating-professional-growth.js
+++ src/pages/case-study/learnerbly-accelerating-professional-growth.js
@@ -154,10 +154,14 @@ const IndexPage = props => {
         </RegalBlueBackground>
       )}
 
+      {/* TODO - handle specific display */}
       {/* "Staying on target" */}
       {shouldRender(data7) && (
         <Grid>
-          <BlockRow mobile={{ bottom: '4', top: '4' }}>
+          <BlockRow
+            mobile={{ bottom: '5', top: '4' }}
+            tablet={{ bottom: '6', top: '6' }}
+          >
             <Col width={[1, 1, 1, 4 / 12]}>
               <ReactMarkdown
                 renderers={{
