1049a3e272b2425a203d2fcfd29ce6e01e9735e0
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-07-19 10:07:19 +0100

    Remove propType from Anchor

--- src/components/Common/Anchor.js
+++ src/components/Common/Anchor.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import ExternalAnchor from './ExternalAnchor'
 
-const Anchor = ({ to, href, activeClassName, ...props }) => {
-  if (to != null) {
-    return <Link to={to} activeClassName={activeClassName} {...props} />
-  } else {
-    return <ExternalAnchor href={href} {...props} />
-  }
-}
-
-Anchor.propTypes = {
-  title: PropTypes.string.isRequired
-}
+const Anchor = ({ to, href, activeClassName, ...props }) =>
+  to != null ? (
+    <Link to={to} activeClassName={activeClassName} {...props} />
+  ) : (
+    <ExternalAnchor href={href} {...props} />
+  )
 
 export default Anchor
