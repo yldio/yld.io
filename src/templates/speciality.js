f3336e9bb389332bb44e69a03687f2c209b3a787
Ania <ania.makes@gmail.com>
2019-07-23 14:57:38 +0100

    attempt to fix case sensitivity issue with Netlify

--- src/templates/speciality.js
+++ src/templates/speciality.js
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import { isAfter, endOfYesterday } from 'date-fns'
 import Get from 'lodash.get'
-import IsNull from 'lodash.isnull'
+import isNull from 'lodash.isnull'
 import Ajv from 'ajv'
 
 import IntroSection from '../components/Speciality/Intro'
@@ -129,13 +129,13 @@ const flattenSpeciality = speciality => {
     eventIconTitle: Get(speciality, 'eventIcon.title', undefined),
     eventIconUrl: Get(speciality, 'eventIcon.file.url', undefined),
     contactText: Get(speciality, 'contactText', undefined), // required
-    externalResources: IsNull(speciality.externalResources)
+    externalResources: isNull(speciality.externalResources)
       ? []
       : Get(speciality, 'externalResources', []),
-    relatedProjects: IsNull(speciality.relatedProjects)
+    relatedProjects: isNull(speciality.relatedProjects)
       ? []
       : Get(speciality, 'relatedProjects', []),
-    clients: IsNull(speciality.clients) ? [] : Get(speciality, 'clients', []),
+    clients: isNull(speciality.clients) ? [] : Get(speciality, 'clients', []),
     eventIcon: Get(speciality, 'eventIcon', undefined)
   }
 }
