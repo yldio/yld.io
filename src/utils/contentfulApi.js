696f10dfa3d48bade06e232493dada71054cabf9
Ollie Monk <ollie@olliemonk.com>
2019-07-24 12:31:13 +0800

    add entry route

--- src/utils/contentfulApi.js
+++ src/utils/contentfulApi.js
@@ -1,19 +1,17 @@
 const { createClient } = require('contentful-management')
-
+const express = require('express')
+const app = express()
+const Intercept = require('apr-intercept')
 const { PORT = 3000, CONTENTFUL_SPACE, CMS_CRUD } = process.env
 
 const client = createClient({
   accessToken: CMS_CRUD
 })
 
-const express = require('express')
-const app = express()
-
-app.get('/', async (req, res) => {
+app.get('/content-type', async (req, res) => {
   const contentType = req.param('contentType')
-  const entryId = req.param('entryId')
 
-  if ([entryId, contentType].every(q => !q)) {
+  if (!contentType) {
     res.send('Missing query params')
   }
 
@@ -27,6 +25,25 @@ app.get('/', async (req, res) => {
 
   res.setHeader('Content-Type', 'application/json')
   res.send(JSON.stringify({ contentType, entries }, null, 2))
+})
+
+app.get('/entry', async (req, res) => {
+  const { id: entryId } = req.query
+
+  const space = await client.getSpace(CONTENTFUL_SPACE)
+  const environment = await space.getEnvironment('master')
+
+  if (!entryId) {
+    return res.send('Missing id query param')
+  }
+
+  const [err, entry] = await Intercept(environment.getEntry(entryId))
+  if (err) {
+    res.send(err)
+  }
+
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify({ entry }, null, 2))
 })
 
 // eslint-disable-next-line no-console
