2443c8d02533595ccc9015a9a97d8aa4394c04cb
Ollie Monk <ollie@olliemonk.com>
2019-08-19 20:44:46 +0800

    revert changes to addon.js

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,60 +1,45 @@
-// import '@storybook/addon-actions/register'
-// import '@storybook/addon-links/register'
-// import '@storybook/addon-a11y/register'
+import '@storybook/addon-actions/register'
+import '@storybook/addon-links/register'
+import '@storybook/addon-a11y/register'
 import registerScissors from 'storybook-addon-scissors'
 
-// const devices = [
-//   {
-//     uid: 'small-mobile',
-//     title: 'Small Mobile',
-//     width: 320,
-//     height: 568
-//   },
-//   {
-//     uid: 'medium-mobile',
-//     title: 'Medium Mobile',
-//     width: 550,
-//     height: 736
-//   },
-//   {
-//     uid: 'large-mobile',
-//     title: 'Large Mobile',
-//     width: 650,
-//     height: 736
-//   },
-//   {
-//     uid: 'small-tablet',
-//     title: 'Small Tablet',
-//     width: 768,
-//     height: 600
-//   },
-//   {
-//     uid: 'tablet',
-//     title: 'Tablet',
-//     width: 1000,
-//     height: 600
-//   },
-//   {
-//     uid: 'desktop',
-//     title: 'Desktop',
-//     width: 1440,
-//     height: 624
-//   }
-// ]
+const devices = [
+  {
+    uid: 'small-mobile',
+    title: 'Small Mobile',
+    width: 320,
+    height: 568
+  },
+  {
+    uid: 'medium-mobile',
+    title: 'Medium Mobile',
+    width: 550,
+    height: 736
+  },
+  {
+    uid: 'large-mobile',
+    title: 'Large Mobile',
+    width: 650,
+    height: 736
+  },
+  {
+    uid: 'small-tablet',
+    title: 'Small Tablet',
+    width: 768,
+    height: 600
+  },
+  {
+    uid: 'tablet',
+    title: 'Tablet',
+    width: 1000,
+    height: 600
+  },
+  {
+    uid: 'desktop',
+    title: 'Desktop',
+    width: 1440,
+    height: 624
+  }
+]
 
-// console.log('------------------------')
-// console.log('------------------------')
-// console.log('------------------------')
-// console.log('------------------------')
-// console.log('------------------------')
-// console.log('------------------------')
-// console.log('------------------------')
-// console.log('------------------------')
-// console.log('------------------------')
-// console.log('------------------------')
-// console.log('------------------------')
-// console.log('------------------------')
-// console.log('------------------------')
-
-// console.log('------------------------')
-registerScissors([])
+registerScissors(devices)
