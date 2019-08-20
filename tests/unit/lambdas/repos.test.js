19333f784c24144e3eb4066d1fb06a60b2d735f1
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-08-20 15:19:01 +0100

    Update repos.test and pass first one

--- tests/unit/lambdas/repos.test.js
+++ tests/unit/lambdas/repos.test.js
@@ -4,145 +4,104 @@ import ReposLambda from '../../../src/functions/oss/repos'
 /* careful: No console log can be used within the test or relevant files
 as long as console log is mocked. To log the mock needs to be disabled */
 // eslint-disable-next-line
-console.log = jest.fn()
+// console.log = jest.fn()
 
-jest.mock('../../../src/functions/oss/utils')
-const {
-  getFieldValue,
-  generateContentfulData
-} = require('../../../src/functions/oss/utils')
+import ossUtils from '../../../src/functions/oss/utils'
+const updateEntrySpy = jest.spyOn(ossUtils, 'updateEntry')
 
-const mockGetFieldValue = () =>
-  getFieldValue.mockImplementation(repo => repo.fields.url['en-US'])
+process.env.LAMBDA_ENV = 'production'
 
-const mockGenerateContentfulData = samaData =>
-  generateContentfulData.mockImplementation(repo => {
-    let contentfulRepo = Object.assign({}, repo)
-    const repoUrl = `${repo.url}${samaData ? '' : '-different'}`
-    delete repo.url
-    contentfulRepo.url = {}
-    contentfulRepo.url['en-US'] = repoUrl
-    return contentfulRepo
-  })
-
-const firstRepo = {
+const sameRepoOne = {
   url: 'https://github.com/yldio/fake-repo',
   nameWithOwner: 'yldio/fake-repo',
-  descriptionHTML: '<div>This is fake! It does not exist!</div>',
-  starCount: 72,
-  pullRequestCount: 3,
-  topics: [],
-  pullRequests: [
-    'MDExOlB1bGxSZXF1ZXN0Nzc1MzkzMDc=',
-    'MDExOlB1bGxSZXF1ZXN0MTM2MzY1MDU5',
-    'MDExOlB1bGxSZXF1ZXN0MTUxNjY4NzY0'
-  ],
-  contributors: ['yldio'],
-  pullRequestsRank: 85,
-  starsRank: 366,
-  rank: 451
+  descriptionHTML: '<div>This is a fake repo.</div>',
+  starCount: 10924,
+  pullRequestCount: 4
 }
 
-const secondRepo = {
+const differentRepoOne = Object.assign({}, sameRepoOne)
+differentRepoOne.pullRequestCount = 6
+
+const sameRepoTwo = {
   url: 'https://github.com/yldio/another-fake-repo',
   nameWithOwner: 'yldio/another-fake-repo',
-  descriptionHTML: '<div>This is fake as well! It does not exist!</div>',
-  starCount: 62,
-  pullRequestCount: 2,
-  topics: [],
-  pullRequests: [
-    'MDExOlB1bGxSZXF1ZXN0MjUzNDI2MDA1',
-    'MDExOlB1bGxSZXF1ZXN0MjUzNDU4MTUx'
-  ],
-  contributors: ['yldio'],
-  pullRequestsRank: 69,
-  starsRank: 123,
-  rank: 294
+  descriptionHTML: '<div>This is another fake repo.</div>',
+  starCount: 8724,
+  pullRequestCount: 3
 }
 
-let environmentRepos = [
-  Object.assign({}, firstRepo),
-  Object.assign({}, secondRepo)
-].map(repo => ({ fields: repo }))
+const contentfulRepoOne = {
+  fields: {
+    url: {
+      'en-US': 'https://github.com/yldio/fake-repo'
+    },
+    nameWithOwner: {
+      'en-US': 'yldio/fake-repo'
+    },
+    descriptionHTML: {
+      'en-US': '<div>This is a fake repo.</div>'
+    },
+    pullRequestCount: {
+      'en-US': 4
+    },
+    starCount: {
+      'en-US': 10924
+    }
+  }
+}
 
-environmentRepos.forEach(repo => {
-  const repoUrl = repo.fields.url
-  delete repo.fields.url
-  repo.fields.url = {}
-  repo.fields.url['en-US'] = repoUrl
-})
+const contentfulRepoTwo = {
+  fields: {
+    url: {
+      'en-US': 'https://github.com/yldio/another-fake-repo'
+    },
+    nameWithOwner: {
+      'en-US': 'yldio/another-fake-repo'
+    },
+    descriptionHTML: {
+      'en-US': '<div>This is another fake repo.</div>'
+    },
+    pullRequestCount: {
+      'en-US': 3
+    },
+    starCount: {
+      'en-US': 8724
+    }
+  }
+}
+
+let mockedContentfulRepos = [contentfulRepoOne, contentfulRepoTwo]
 
 describe('Repos lambda', () => {
-  let newReposResponse
+  let mockedEnvironment
 
   beforeEach(() => {
-    newReposResponse = [
-      Object.assign({}, firstRepo),
-      Object.assign({}, secondRepo)
-    ]
+    mockedEnvironment = {
+      getEntries: jest.fn().mockReturnValue({ items: mockedContentfulRepos })
+    }
   })
 
   afterEach(() => {
     jest.clearAllMocks()
   })
 
-  describe('is not production environment', () => {
-    it(`with equal fields, should not update and log the 'Fields have not changed' message`, async () => {
-      mockGetFieldValue()
-      mockGenerateContentfulData(true)
-
-      const mockedEnvironment = {
-        getEntries: jest.fn().mockReturnValue({ items: environmentRepos })
-      }
-
+  /* We know that the only two values that can change the outcome of the script
+  are the `repos` and `contentfulRepos` */
+  describe('with repos and contentfulRepos having the same values', () => {
+    it('should not call updateEntry and no changes should be returned', async () => {
       const response = await ReposLambda(mockedEnvironment, {
-        repos: newReposResponse
+        repos: [sameRepoOne, sameRepoTwo]
       })
 
-      // eslint-disable-next-line
-      expect(console.log.mock.calls[0][0]).toBe(
-        `Fields for ${firstRepo.nameWithOwner} have not changed. Not updating!`
-      )
+      expect(updateEntrySpy).not.toHaveBeenCalled()
 
-      // eslint-disable-next-line
-      expect(console.log.mock.calls[1][0]).toBe(
-        `Fields for ${secondRepo.nameWithOwner} have not changed. Not updating!`
-      )
-      expect(response).toEqual([])
-    })
+      const noChangesExpected = []
 
-    it(`with different fields, should not update and log the 'Not Prod' message`, async () => {
-      mockGetFieldValue()
-      mockGenerateContentfulData(false)
-
-      const mockedEnvironment = {
-        getEntries: jest.fn().mockReturnValue({ items: environmentRepos })
-      }
-
-      const response = await ReposLambda(mockedEnvironment, {
-        repos: newReposResponse
-      })
-
-      // eslint-disable-next-line
-      expect(console.log.mock.calls[0][0]).toBe(
-        `Not prod so not updating contentful for ${firstRepo.nameWithOwner}`
-      )
-
-      // eslint-disable-next-line
-      expect(console.log.mock.calls[1][0]).toBe(
-        `Not prod so not updating contentful for ${secondRepo.nameWithOwner}`
-      )
-      expect(response).toEqual([])
+      expect(response).toStrictEqual(noChangesExpected)
     })
   })
 
-  describe.skip('is production environment', () => {
-    it('with equal fields, should not update  and log the have not changed fields message', async () => {
-      //   expect().toEqual(`Fields for ${repoMock} have not changed. Not updating!`)
-    })
-
-    it('with different fields, should update entry and not log any message', async () => {
-      //   expect().toEqual(`Fields for ${repoMock} have not changed. Not updating!`)
-    })
+  describe('with repos and contentfulRepos having different values', () => {
+    it('should call updateEntry and return the changes', async () => {})
   })
 })
