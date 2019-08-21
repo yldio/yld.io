7708d0774b1947ccb5029d89409102b6c54c3f24
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-08-21 16:35:19 +0100

    Add Meta test for different meta data values

--- tests/unit/lambdas/meta.test.js
+++ tests/unit/lambdas/meta.test.js
@@ -6,11 +6,6 @@ ossUtils.updateEntry = jest.fn().mockImplementation(() => Promise.resolve(null))
 
 process.env.LAMBDA_ENV = 'production'
 
-const sameGithubMetaData = {
-  openSourceMetaPullRequestsCount: 3886,
-  openSourceMetaReposCount: 1001
-}
-
 const contentfulMetasMock = [
   {
     fields: {
@@ -34,11 +29,40 @@ describe('Github lambda - Meta util', () => {
   })
 
   it('should not call updateEntry and should return the expected data if github and contentful MetaDatas are not different', async () => {
+    const sameGithubMetaData = {
+      openSourceMetaPullRequestsCount: 3886,
+      openSourceMetaReposCount: 1001
+    }
+
     const response = await MetaUtil(mockedEnvironment, sameGithubMetaData)
 
     expect(ossUtils.updateEntry).not.toHaveBeenCalled()
 
     const expected = { ...sameGithubMetaData }
+    expect(response).toStrictEqual(expected)
+  })
+
+  it('should call updateEntry and should return the expected data if github and contentful MetaDatas are different', async () => {
+    const differentGithubMetaData = {
+      openSourceMetaPullRequestsCount: 3999,
+      openSourceMetaReposCount: 1017
+    }
+
+    const expectedContentfulData = {
+      openSourceMetaPullRequestsCount: { 'en-US': 3999 },
+      openSourceMetaReposCount: { 'en-US': 1017 }
+    }
+
+    const response = await MetaUtil(mockedEnvironment, differentGithubMetaData)
+
+    expect(ossUtils.updateEntry).toHaveBeenCalledWith(
+      contentfulMetasMock[0],
+      expectedContentfulData,
+      mockedEnvironment,
+      'github meta data'
+    )
+
+    const expected = { ...differentGithubMetaData }
     expect(response).toStrictEqual(expected)
   })
 })
