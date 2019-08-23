9b0f54cdd31babe6b68b53f9a64e532d35979a75
Fábio Moreira <fabiommmoreira@gmail.com>
2019-08-23 10:48:05 +0100

    Unit tests (#322)
    * unit test config and 1st test
    
    * remove testing-library
    
    * specility Intro and caseStudy title links
    
    * components/About_Us/AboutUsHero
    
    * components/AboutUs/ClientTestimonial
    
    * components/AboutUs/Partners
    
    * components/AboutUs/StaffCard
    
    * components/AboutUs/Subsidiaries
    
    * react-testing-library and Teams test
    
    * remove coverage folder
    
    * add coverage folder to gitignore
    
    * use testing-library on previous tests
    
    * update snapshots
    
    * revert fix
    
    * create separate jest config file for unit tests
    
    * add unit tests to circleci
    
    * update snapshots
    
    * add interaction tests to snapshot task
    
    * remove duplicate snapshots task
    
    * rename jest config file and prop spreading

--- stories/AboutUs.stories.js
+++ stories/AboutUs.stories.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { storiesOf, addDecorator } from '@storybook/react'
 import Theme from './theme'
-import Teams from '../src/components/AboutUs/Teams'
-import { teams } from './assets/about-us-data.js'
-
-const data = {
-  title: 'Our teams',
-  teams: teams
-}
+import AboutUsHero from '../src/components/AboutUs/AboutUsHero'
+import ClientTestimonial from '../src/components/AboutUs/ClientTestimonial'
+import Partners from '../src/components/AboutUs/Partners'
+import StaffCard from '../src/components/AboutUs/StaffCard'
+import Subsidiaries from '../src/components/AboutUs/Subsidiaries'
+import {
+  statements,
+  clientTestimonial,
+  partners,
+  teamMember,
+  subsidiaries
+} from './assets/about-us-data.js'
 
 addDecorator(Theme)
 
-storiesOf('AboutUs', module).add('Teams', () => <Teams {...data} />)
+storiesOf('AboutUs', module)
+  .add('AboutUsHero', () => <AboutUsHero {...statements} />)
+  .add('ClientTestimonial', () => <ClientTestimonial {...clientTestimonial} />)
+  .add('Partners', () => <Partners {...partners} />)
+  .add('StaffCard', () => <StaffCard {...teamMember} />)
+  .add('Subsidiaries', () => <Subsidiaries {...subsidiaries} />)
