9498e1c8dce0c2a23a8a0993c60db14246d006fc
Ollie Monk <ollie@olliemonk.com>
2019-08-28 12:50:05 +0800

    remove sharp plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sitemap`,
