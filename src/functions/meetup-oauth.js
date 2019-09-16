d13164226453f6da520529647754abbcf08e0023
Ollie Monk <ollie@olliemonk.com>
2019-09-16 16:14:21 +0800

    update meetup lambda to use session flow, update readme

--- src/functions/meetup-oauth.js
+++ src/functions/meetup-oauth.js
@@ -1,33 +1,19 @@
-const { MEETUP_API_SECRET, MEETUP_API_KEY } = process.env
+const Got = require('got')
 
-const meetup = require('meetup-api')({
-  oauth: {
-    key: MEETUP_API_KEY,
-    secret: MEETUP_API_SECRET
-  }
-})
-
-const { LAMBDA_ENV } = process.env
+const { MEETUP_API_KEY, LAMBDA_ENV } = process.env
 const isProd = LAMBDA_ENV === 'production'
+
 const redirect = `${
   isProd ? 'https://yld.io/.netlify/functions' : 'http://localhost:9000'
 }/meetup-callback`
 
-const params = {
-  redirect
-}
+exports.handler = async () => {
+  const { body } = await Got(
+    `https://secure.meetup.com/oauth2/authorize?client_id=${MEETUP_API_KEY}&redirect_uri=${redirect}&response_type=anonymous_code`
+  )
 
-exports.handler = (evt, ctx, callback) => {
-  return meetup.getOAuth2RequestToken(params, (err, res) => {
-    if (err) {
-      throw new Error(err)
-    }
-
-    return callback(null, {
-      statusCode: 301,
-      headers: {
-        Location: res
-      }
-    })
-  })
+  return {
+    statusCode: 200,
+    body
+  }
 }
