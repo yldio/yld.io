197f55ccd73eba3d0128ff0e6c4b4dbf902ae0b5
Ollie Monk <ollie@olliemonk.com>
2019-09-16 12:03:48 +0800

    refresh token on each start

--- src/functions/meetup-callback.js
+++ src/functions/meetup-callback.js
@@ -1,38 +1,19 @@
-const { MEETUP_API_SECRET, MEETUP_API_KEY } = process.env
+const { MEETUP_API_SECRET, MEETUP_API_KEY, MEETUP_REFRESH_TOKEN } = process.env
 
-const meetup = require('meetup-api')({
-  oauth: {
-    key: MEETUP_API_KEY,
-    secret: MEETUP_API_SECRET
-  }
-})
+const Got = require('got')
 
-const { LAMBDA_ENV } = process.env
-const isProd = LAMBDA_ENV === 'production'
-const redirect = `${
-  isProd ? 'https://yld.io/.netlify/functions' : 'http://localhost:9000'
-}/meetup-callback`
-
-const params = {
-  redirect
-}
-
-exports.handler = (evt, ctx, callback) => {
-  const {
-    queryStringParameters: { code }
-  } = evt
-
-  meetup.getOAuth2RequestToken(params, err => {
-    if (err) {
-      throw new Error(err)
-    }
-
-    meetup.getOAuth2AccessToken(code, (...rest) => {
-      // const groups = meetup.getSelfGroups()
-
-      callback(null, {
-        statusCode: 200
-      })
-    })
+exports.handler = async () => {
+  const { body } = await Got.post('https://secure.meetup.com/oauth2/access', {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded '
+    },
+    body: `client_id=${MEETUP_API_KEY}&client_secret=${MEETUP_API_SECRET}&grant_type=refresh_token&refresh_token=${MEETUP_REFRESH_TOKEN}`
   })
+
+  const { access_token } = JSON.parse(body)
+
+  return {
+    statusCode: 200,
+    body: 'yessir'
+  }
 }
