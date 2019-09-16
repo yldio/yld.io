0559613513f5c4db72f7fd90961824668d697a5e
Ollie Monk <ollie@olliemonk.com>
2019-09-16 16:40:37 +0800

    add basic Auth to oauth endpoint

--- src/functions/meetup-oauth.js
+++ src/functions/meetup-oauth.js
@@ -1,4 +1,5 @@
 const Got = require('got')
+const Auth = require('./utils/auth')
 
 const { MEETUP_API_KEY, LAMBDA_ENV } = process.env
 const isProd = LAMBDA_ENV === 'production'
@@ -7,19 +8,20 @@ const redirect = `${
   isProd ? 'https://yld.io/.netlify/functions' : 'http://localhost:9000'
 }/meetup-callback`
 
-exports.handler = async () => {
-  const searchParams = new URLSearchParams([
-    ['client_id', MEETUP_API_KEY],
-    ['redirect_uri', redirect],
-    ['response_type', 'anonymous_code']
-  ])
+exports.handler = async evt =>
+  Auth(evt, async () => {
+    const searchParams = new URLSearchParams([
+      ['client_id', MEETUP_API_KEY],
+      ['redirect_uri', redirect],
+      ['response_type', 'anonymous_code']
+    ])
 
-  const { body } = await Got(
-    `https://secure.meetup.com/oauth2/authorize?${searchParams.toString()}`
-  )
+    const { body } = await Got(
+      `https://secure.meetup.com/oauth2/authorize?${searchParams.toString()}`
+    )
 
-  return {
-    statusCode: 200,
-    body
-  }
-}
+    return {
+      statusCode: 200,
+      body
+    }
+  })
