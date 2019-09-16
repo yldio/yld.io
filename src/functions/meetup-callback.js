2c4692ec8735640068eff07a23d8f8c5adb65798
Ollie Monk <ollie@olliemonk.com>
2019-09-16 12:18:25 +0800

    add AuthenticatedRequest function

--- src/functions/meetup-callback.js
+++ src/functions/meetup-callback.js
@@ -2,6 +2,12 @@ const { MEETUP_API_SECRET, MEETUP_API_KEY, MEETUP_REFRESH_TOKEN } = process.env
 
 const Got = require('got')
 
+const createAuthenticatedRequest = access_token => (url, options = {}) =>
+  Got(url, {
+    ...options,
+    headers: { ...options.headers, Authorization: `Bearer ${access_token}` }
+  })
+
 exports.handler = async () => {
   const { body } = await Got.post('https://secure.meetup.com/oauth2/access', {
     headers: {
@@ -11,9 +17,16 @@ exports.handler = async () => {
   })
 
   const { access_token } = JSON.parse(body)
+  const AuthenticatedRequest = createAuthenticatedRequest(access_token)
+
+  const { body: groups } = await AuthenticatedRequest(
+    'https://api.meetup.com/self/groups'
+  )
+
+  console.log({ groups })
 
   return {
     statusCode: 200,
-    body: 'yessir'
+    body: JSON.stringify({ groups })
   }
 }
