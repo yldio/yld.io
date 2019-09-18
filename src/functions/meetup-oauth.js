eeab469d6219117ddf31e161bf9388e851916742
Ollie Monk <omonk@users.noreply.github.com>
2019-09-18 14:23:58 +0800

    638 meetup oauth 2 (#377)
    * init meetup oauth
    
    * remove secrets 🤦
    
    * refresh token on each start
    
    * add AuthenticatedRequest function
    
    * update callback function with oauth and tidied meetup
    
    * update meetup lambda to use session flow, update readme
    
    * remove meetup.js, update search params in oauth step
    
    * add basic Auth to oauth endpoint
    
    * add type field - default to meetup
    
    * update yarn.lock
    
    * add addressLine(N), city, venue name
    
    * streamline meetup lambda, remove double transformation

---
+++ src/functions/meetup-oauth.js
@@ -0,0 +1,27 @@
+const Got = require('got')
+const Auth = require('./utils/auth')
+
+const { MEETUP_API_KEY, LAMBDA_ENV } = process.env
+const isProd = LAMBDA_ENV === 'production'
+
+const redirect = `${
+  isProd ? 'https://yld.io/.netlify/functions' : 'http://localhost:9000'
+}/meetup-callback`
+
+exports.handler = async evt =>
+  Auth(evt, async () => {
+    const searchParams = new URLSearchParams([
+      ['client_id', MEETUP_API_KEY],
+      ['redirect_uri', redirect],
+      ['response_type', 'anonymous_code']
+    ])
+
+    const { body } = await Got(
+      `https://secure.meetup.com/oauth2/authorize?${searchParams.toString()}`
+    )
+
+    return {
+      statusCode: 200,
+      body
+    }
+  })
