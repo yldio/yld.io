f834690ffd503e6b99777721b389d738f02b5544
Ollie Monk <ollie@olliemonk.com>
2019-09-23 11:26:32 +0800

    services update

--- src/components/Homepage/services.js
+++ src/components/Homepage/services.js
@@ -10,7 +10,7 @@ import Image from '../Common/Image'
 
 import SeoLinks from '../Common/seoLinks'
 import Statement from '../Common/Statement'
-import { DisplayTitle, BodyPrimary } from '../Typography'
+import { DisplayTitle, CardTitle, BodyPrimary } from '../Typography'
 
 const ImageWrapper = styled.div`
   width: 60px;
@@ -21,10 +21,14 @@ const ImageWrapper = styled.div`
 
 const StyledRow = styled(Row)`
   padding-top: ${({ theme }) => theme.space[6]};
+  padding-bottom: ${({ theme }) => theme.space[6]};
+`
+
+const ServiceRow = styled(Row)`
   padding-bottom: ${({ theme }) => theme.space[5]};
 `
 
-const ServiceTitle = styled(DisplayTitle)`
+const ServiceTitle = styled(CardTitle)`
   text-decoration: underline;
 `
 
@@ -33,36 +37,40 @@ const ServiceCol = styled(Col)`
 `
 
 const Services = ({ statement, services }) => (
-  <StyledRow>
-    {statement && (
-      <Col width={[1, 1, 1, 10 / 12]}>
-        <DisplayTitle>{statement}</DisplayTitle>
-      </Col>
-    )}
-    {services.map(
-      service =>
-        service.introSentence && (
-          <>
-            <ServiceCol width={[1, 1, 1, 1 / 2, 5 / 12]}>
-              {service.icon && (
-                <ImageWrapper>
-                  <Image image={service.icon} />
-                </ImageWrapper>
-              )}
-              <ServiceTitle>
-                <Link to={`/${service.slug}`}>{service.title}</Link>
-              </ServiceTitle>
+  <>
+    <StyledRow>
+      {statement && (
+        <Col width={[1, 1, 1, 7 / 12]}>
+          <DisplayTitle>{statement}</DisplayTitle>
+        </Col>
+      )}
+    </StyledRow>
+    <ServiceRow>
+      {services.map(
+        service =>
+          service.introSentence && (
+            <>
+              <ServiceCol width={[1, 1, 1, 1 / 2, 5 / 12]}>
+                {service.icon && (
+                  <ImageWrapper>
+                    <Image image={service.icon} />
+                  </ImageWrapper>
+                )}
+                <ServiceTitle noPadding>
+                  <Link to={`/${service.slug}`}>{service.title}</Link>
+                </ServiceTitle>
 
-              <BodyPrimary>{service.introSentence.introSentence}</BodyPrimary>
+                <BodyPrimary>{service.introSentence.introSentence}</BodyPrimary>
 
-              <SeoLinks items={service.homePageSpecialities} />
-            </ServiceCol>
-            {/* This grid has no offsets so this is what we're left with... */}
-            <Col width={[0, 0, 0, 1 / 12]} />
-          </>
-        )
-    )}
-  </StyledRow>
+                <SeoLinks items={service.homePageSpecialities} />
+              </ServiceCol>
+              {/* This grid has no offsets so this is what we're left with... */}
+              <Col width={[0, 0, 0, 1 / 12]} />
+            </>
+          )
+      )}
+    </ServiceRow>
+  </>
 )
 
 export default Services
