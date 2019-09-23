e7013ca2ebb48d780e08becb5bb20ef6ac3b015f
Ollie Monk <ollie@olliemonk.com>
2019-09-23 16:24:17 +0800

    breakpoints for event type section

--- src/components/Homepage/events/index.js
+++ src/components/Homepage/events/index.js
@@ -11,30 +11,16 @@ import breakpoint from 'styled-components-breakpoint'
 import FeaturedEventRow from './FeaturedEventRow'
 import Hr from '../../Common/Hr'
 
-const eventTypes = [
-  {
-    title: 'Conferences',
-    copy:
-      'Knowledge-packed talks from some of the communities best and brightest.'
-  },
-  {
-    title: 'Meet ups',
-    copy: 'Regular gatherings for both beginners and advanced tech enthusiasts.'
-  },
-  {
-    title: 'Workshops',
-    copy:
-      'Offering crash courses and technical seesions to expand your digital skills.'
-  }
-]
-
-const IntroCol = styled(Col)`
-  padding-top: ${({ theme }) => theme.space[5]};
-  padding-bottom: ${({ theme }) => theme.space[4]};
-`
-
 const EventType = styled(Col)`
-  padding-bottom: ${({ theme }) => theme.space[6]};
+  padding-bottom: ${({ theme }) => theme.space[4]};
+
+  ${breakpoint('smallTablet')`
+    padding-bottom: ${({ theme }) => theme.space[5]};
+  `}
+
+  ${breakpoint('tablet')`
+    padding-bottom: ${({ theme }) => theme.space[6]};
+  `}
 `
 
 const EventTypeCopyWrapper = styled.div`
@@ -43,37 +29,62 @@ const EventTypeCopyWrapper = styled.div`
 `
 
 const IntroRow = styled(Row)`
-  padding-bottom: ${({ theme }) => theme.space[7]};
+  padding-top: ${({ theme }) => theme.space[4]};
+  padding-bottom: ${({ theme }) => theme.space[4]};
+
+  ${breakpoint('tablet')`
+    padding-top: ${({ theme }) => theme.space[5]};
+    padding-bottom: ${({ theme }) => theme.space[4]};
+  `}
 `
 
-const Events = ({ featuredEvent }) => (
+const EventTypesRow = styled(Row)`
+  /* padding-bottom: ${({ theme }) => theme.space[6]}; */
+`
+
+const CtaRow = styled(Row)`
+  padding-bottom: ${({ theme }) => theme.space[5]};
+
+  ${breakpoint('tablet')`
+    padding-bottom: ${({ theme }) => theme.space[7]};  
+  `}
+`
+
+const Events = ({ featuredEvent, eventTypes }) => (
   <Grid>
     <FeaturedEventRow event={featuredEvent} />
     <Hr />
     <IntroRow>
-      <IntroCol>
+      <Col width={[1, 1, 1, 1, 8 / 12, 8 / 12, 5 / 12]}>
         <DisplayTitle>Our event types</DisplayTitle>
         <BodyPrimary>
           We{"'"}re proud to offer a variety of different YLD-run events, so
           whether you{"'"}re taking your first steps in tech or are a seasoned
           veteran, there{"'"}s something for everyone.
         </BodyPrimary>
-      </IntroCol>
+      </Col>
+    </IntroRow>
+    <EventTypesRow>
       {eventTypes &&
         eventTypes.length > 0 &&
         eventTypes.map(({ title, copy, image }) => (
-          <EventType width={[1, 1, 1, 4 / 12]} key={generate()}>
+          <EventType
+            width={[1, 1, 1, 1, 6 / 12, 6 / 12, 4 / 12]}
+            key={generate()}
+          >
             <EventTypeCopyWrapper>
               <Subtitle noPadding>{title}</Subtitle>
               <BodyPrimary muted>{copy}</BodyPrimary>
-              {image && <Image image={image} />}
             </EventTypeCopyWrapper>
+            {image && <Image image={image} />}
           </EventType>
         ))}
+    </EventTypesRow>
+    <CtaRow>
       <Col width={[1]}>
         <StyledLink>See all our events</StyledLink>
       </Col>
-    </IntroRow>
+    </CtaRow>
   </Grid>
 )
 
