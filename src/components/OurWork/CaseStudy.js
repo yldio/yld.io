d7b6a5316bd21735afd4e3eb2833aaa261eb1f60
Fábio Moreira <fabiommmoreira@gmail.com>
2019-09-23 11:59:05 +0100

    split homepage ourwork from ourwork page

--- src/components/OurWork/CaseStudy.js
+++ src/components/OurWork/CaseStudy.js
@@ -11,6 +11,49 @@ import { CardTitle, BodyPrimary } from '../Typography'
 import getIntroSentence from '../../utils/getIntroSentence'
 import eventLabels from '../../utils/eventLabels'
 
+const MobileOnlyCol = styled(Col)`
+  ${breakpoint('smallTablet')`
+    display: none;
+  `}
+`
+
+const NonMobileCol = styled(Col)`
+  display: none;
+
+  ${breakpoint('smallTablet')`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+  `}
+`
+
+const serviceLinkMapper = {
+  Engineering: '/engineering',
+  Design: '/design',
+  Training: '/training'
+}
+
+const ServiceList = ({ services }) =>
+  services.map((service, index) => {
+    const isPenultimate = index === services.length - 2
+    const isLast = index === services.length - 1
+    const link = serviceLinkMapper[service]
+    const element = link ? <Anchor to={link}>{service}</Anchor> : service
+
+    switch (true) {
+      case isLast:
+        return <Fragment key={index}>{element}</Fragment>
+      case isPenultimate:
+        // anchor/text + ampersand
+        return <Fragment key={index}>{element} &amp; </Fragment>
+      default:
+        // anchor/text + comma
+        return <Fragment key={index}>{element} &#44; </Fragment>
+    }
+  })
+
 const TitleSection = ({ services, title, link }) => {
   return (
     <Padding bottom={{ smallPhone: 1, smallTablet: 0 }}>
@@ -40,66 +83,41 @@ const InfoSection = ({ introSentence, title, link }) => (
   </Fragment>
 )
 
-const Card = styled.div`
-  width: 100%;
-  height: 0;
-  padding-top: ${({ theme }) => theme.space[4]};
-  padding-top: 75%;
-  background-color: #${({ posterColor }) => posterColor || 'FFFF00'};
-  background-image: url(${({ posterImage }) => posterImage.url});
-  background-position: center bottom;
-  background-size: 100% auto;
-  background-repeat: no-repeat;
-
-  ${breakpoint('tablet')`
-    width: calc(50% - ${({ theme }) => theme.space[3]});
-    margin-top: ${({ theme }) => theme.space[5]};
-  `}
-
-  ${breakpoint('desktop')`
-    width: calc(calc(100% / 3) - ${({ theme }) => theme.spacing[32]});
-  `}
-`
-
 const CaseStudy = ({ caseStudy }) => {
-  console.log('caseStyudy', caseStudy)
-  const { title, services, posterImage, slug, posterColor } = caseStudy
+  const { title, services, posterImage, slug } = caseStudy
   const introSentence = getIntroSentence(caseStudy)
   const caseStudyLink = `/case-study/${slug}`
 
-  return <Card posterColor={posterColor} posterImage={posterImage.file} />
-
-  return <Image alt={posterImage.title} image={posterImage} />
-
-  // return (
-  //   <Row>
-  //     <MobileOnlyCol width={[1, 1, 1, 1, 0, 0, 0]}>
-  //       <TitleSection services={services} title={title} link={caseStudyLink} />
-  //     </MobileOnlyCol>
-  //     <Col width={[1, 1, 1, 1, 5 / 12, 4 / 12, 4 / 12]}>
-  //       <Padding bottom={{ smallPhone: 1, smallTablet: 0 }}>
-  //         <Anchor to={caseStudyLink}>
-  //           <Image alt={posterImage.title} image={posterImage} />
-  //         </Anchor>
-  //       </Padding>
-  //     </Col>
-  //     <NonMobileCol width={[0, 0, 0, 0, 7 / 12, 6 / 12]}>
-  //       <TitleSection services={services} title={title} link={caseStudyLink} />
-  //       <InfoSection
-  //         introSentence={introSentence}
-  //         title={title}
-  //         link={caseStudyLink}
-  //       />
-  //     </NonMobileCol>
-  //     <MobileOnlyCol width={[1, 1, 1, 1, 0, 0, 0]}>
-  //       <InfoSection
-  //         introSentence={introSentence}
-  //         title={title}
-  //         link={caseStudyLink}
-  //       />
-  //     </MobileOnlyCol>
-  //   // </Row>
-  // )
+  return (
+    <Row>
+      <MobileOnlyCol width={[1, 1, 1, 1, 0, 0, 0]}>
+        <TitleSection services={services} title={title} link={caseStudyLink} />
+      </MobileOnlyCol>
+      <Col width={[1, 1, 1, 1, 5 / 12, 4 / 12, 4 / 12]}>
+        <Padding bottom={{ smallPhone: 1, smallTablet: 0 }}>
+          <Anchor to={caseStudyLink}>
+            <Image alt={posterImage.title} image={posterImage} />
+          </Anchor>
+        </Padding>
+      </Col>
+      <NonMobileCol width={[0, 0, 0, 0, 7 / 12, 6 / 12]}>
+        <TitleSection services={services} title={title} link={caseStudyLink} />
+        <InfoSection
+          introSentence={introSentence}
+          title={title}
+          link={caseStudyLink}
+        />
+      </NonMobileCol>
+      <MobileOnlyCol width={[1, 1, 1, 1, 0, 0, 0]}>
+        <InfoSection
+          introSentence={introSentence}
+          title={title}
+          link={caseStudyLink}
+        />
+      </MobileOnlyCol>
+    </Row>
+  )
 }
 
 export default CaseStudy
+export { CaseStudy, ServiceList }
---
+++ src/components/OurWork/hue.js
@@ -0,0 +1,157 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+import remcalc from 'remcalc'
+
+import { Row, Col } from '../grid'
+import { Padding } from 'styled-components-spacing'
+import breakpoint from 'styled-components-breakpoint'
+import Image from '../Common/Image'
+import StyledLink from '../Common/StyledLink'
+import SeoLinks from '../Common/seoLinks'
+import { SectionTitle, CardTitle, BodyPrimary } from '../Typography'
+import eventLabels from '../../utils/eventLabels'
+
+const CardHeader = styled.header`
+  padding: ${remcalc(24)} ${remcalc(36)} ${remcalc(22)} ${remcalc(36)};
+  max-width: ${remcalc(475)};
+  box-sizing: border-box;
+
+  > div {
+    max-width: ${remcalc(310)};
+  }
+`
+
+const AnimatedLink = styled(Link)`
+  > section {
+    transition: transform ${props => props.theme.animations.normal} ease;
+  }
+
+  &:focus,
+  &:hover {
+    > section {
+      transform: scale(0.97);
+    }
+  }
+`
+
+const ImageWrapper = styled.div`
+  background: #${props => props.color};
+  max-width: 100%;
+`
+
+const MasonryContainer = styled(Col)`
+  box-sizing: border-box;
+  max-width: 100%;
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: column;
+
+  ${breakpoint('smallTablet')`
+    display: block;
+    column-count: 2;
+    column-gap: 49px;
+  `}
+`
+
+const orderMap = {
+  0: 1,
+  1: 3,
+  2: 2,
+  3: 4
+}
+
+const MasonryElement = styled.div`
+  break-inside: avoid;
+  order: ${({ index }) => orderMap[index]};
+
+  ${breakpoint('desktop')`
+    order: initial;
+  `};
+`
+
+const Services = ({ services }) => (
+  <Row pt={[3.5, 3.5, 3.5, 5]}>
+    <MasonryContainer width={[1]}>
+      {services.map(
+        (service, index, arr) =>
+          service.introSentence && (
+            <MasonryElement key={service.id} index={index}>
+              {((arr.length === 2 && index === 1) ||
+                (arr.length !== 2 && index === 2)) && (
+                <Padding
+                  top={{ smallPhone: 0, smallTablet: 6.5 }}
+                  bottom={{ smallPhone: 0, smallTablet: 2 }}
+                />
+              )}
+
+              <SectionTitle>
+                {service.pageReady ? (
+                  <Link to={`/${service.slug}`}>{service.title}</Link>
+                ) : (
+                  service.title
+                )}
+              </SectionTitle>
+
+              <Padding top={1.5} bottom={0.5}>
+                <BodyPrimary>{service.introSentence.introSentence}</BodyPrimary>
+              </Padding>
+              {service.homePageSpecialities &&
+                service.homePageSpecialities.length && (
+                  <Row>
+                    <Col width={[1, 1, 1, 1, 1, 9 / 12]}>
+                      <SeoLinks items={service.homePageSpecialities} />
+                    </Col>
+                  </Row>
+                )}
+
+              {service.pageReady ? (
+                <Padding bottom={1.5}>
+                  <StyledLink
+                    to={`/${service.slug}`}
+                    title={`Learn more about our ${service.title} service`}
+                    data-event={`${eventLabels.homepageLearnMore}-${
+                      service.title
+                    }`}
+                  >
+                    Learn more
+                  </StyledLink>
+                </Padding>
+              ) : (
+                <Padding bottom={1.5} />
+              )}
+
+              <AnimatedLink
+                to={`/case-study/${service.caseStudies[0].slug}`}
+                title={service.caseStudies[0].title}
+              >
+                <section
+                  style={{
+                    background: `#${service.caseStudies[0].posterColor}`
+                  }}
+                >
+                  <CardHeader>
+                    <section>
+                      <BodyPrimary reverse muted>
+                        Case study
+                      </BodyPrimary>
+                      <CardTitle reverse noPadding>
+                        {service.caseStudies[0].title}
+                      </CardTitle>
+                    </section>
+                  </CardHeader>
+                  <ImageWrapper color={service.caseStudies[0].posterColor}>
+                    <Image image={service.caseStudies[0].posterImage} />
+                  </ImageWrapper>
+                </section>
+              </AnimatedLink>
+
+              <Padding bottom={{ smallPhone: 4, smallTablet: 5 }} />
+            </MasonryElement>
+          )
+      )}
+    </MasonryContainer>
+  </Row>
+)
+
+export default Services
