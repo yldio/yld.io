d8082d7c2d51f2cf01269fb7c8f02552f2db212c
Ollie Monk <ollie@olliemonk.com>
2019-10-21 12:27:58 +0800

    update main export tests

--- tests/unit/lambdas/blogMethods/main-export.test.js
+++ tests/unit/lambdas/blogMethods/main-export.test.js
@@ -1,11 +1,21 @@
+import ParseXMLtoJSON from '../../../../src/functions/blogMethods/parse-xml-to-json'
+import ParseHTMLToMarkdown from '../../../../src/functions/blogMethods/parse-html-to-markdown'
+import TransformCustomMDX from '../../../../src/functions/blogMethods/transform-custom-mdx'
+import PublishToContentful from '../../../../src/functions/blogMethods/publish-to-contentful'
+import TranspileAllPosts from '../../../../src/functions/blogMethods/transpile-all-posts'
+import TransformMetaDat from '../../../../src/functions/blogMethods/transform-meta-data'
+
 const MainExport = require('../../../../src/functions/blogMethods')
 jest.mock('../../../../src/functions/utils/is-prod', () => false)
 // eslint-disable-next-line no-unused-vars
 const contentful = require('contentful-management')
 
 const mockGetEntries = jest.fn()
+const mockGetContentType = jest.fn()
+
 const mockGetEnvironment = jest.fn(() => ({
-  getEntries: mockGetEntries
+  getEntries: mockGetEntries,
+  getContentType: mockGetContentType
 }))
 
 jest.mock('contentful-management', () => ({
@@ -16,12 +26,7 @@ jest.mock('contentful-management', () => ({
   }))
 }))
 
-const mockParseXMLToJSON = jest.fn(a => a)
-
-jest.mock(
-  '../../../../src/functions/blogMethods/parse-xml-to-json',
-  () => mockParseXMLToJSON
-)
+jest.mock('../../../../src/functions/blogMethods/parse-xml-to-json')
 jest.mock('../../../../src/functions/blogMethods/parse-html-to-markdown')
 jest.mock('../../../../src/functions/blogMethods/transform-custom-mdx')
 jest.mock('../../../../src/functions/blogMethods/publish-to-contentful')
@@ -30,9 +35,49 @@ jest.mock('../../../../src/functions/blogMethods/transform-meta-data')
 
 describe('MainExport', () => {
   it('should run correctly', async () => {
-    console.log({ MainExport })
+    mockGetEntries.mockResolvedValueOnce({
+      items: [
+        {
+          sys: {
+            id: 'post_id'
+          },
+          fields: {
+            title: {
+              'en-US': 'title'
+            }
+          }
+        }
+      ]
+    })
 
-    const result = await MainExport()
+    mockGetContentType.mockResolvedValueOnce({
+      fields: [
+        {
+          id: 'title',
+          required: true
+        },
+        {
+          id: 'authorName',
+          required: true
+        },
+        {
+          id: 'slug',
+          required: true
+        },
+        {
+          id: 'content',
+          required: true
+        }
+      ]
+    })
+
+    ParseXMLtoJSON.mockResolvedValueOnce({
+      title: 'title'
+    })
+
+    const result = await MainExport(['data'])
+
+    expect(mockGetContentType).toHaveBeenCalledWith('blogPost')
   })
 
   it('should throw an error when there is missing data', async () => {
