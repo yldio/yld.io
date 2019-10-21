bceea5f8d15cf47ab44e982956cd997c6ab8d5e6
Tim Seckinger <seckinger.tim@gmail.com>
2019-10-21 16:35:47 +0100

    remove @testing-library/react abstraction layer

---
+++ src/utils/tests/react.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import theme from '../theme'
+
+const AllTheProviders = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
+
+export const wrapper = AllTheProviders
