bbb8e1187bf5e95a60581fc87a7dbf7fb7018bcc
Ollie Monk <ollie@olliemonk.com>
2019-11-05 12:05:30 +0000

    create get content type fields util

---
+++ src/functions/utils/get-content-type-fields.js
@@ -0,0 +1,8 @@
+module.exports = ct =>
+  ct.fields.reduce(
+    ({ allFields = [], requiredFields = [] }, { required, id }) => ({
+      allFields: allFields.concat(id),
+      requiredFields: required ? requiredFields.concat(id) : requiredFields
+    }),
+    []
+  )
