4d13ef7db84fce128de4bed54bd3ebe572ac0644
Tim Seckinger <tim.seckinger@yld.io>
2020-03-30 16:48:06 +0200

    stabilize lighthouse

--- tests/lighthouse/run.js
+++ tests/lighthouse/run.js
@@ -9,7 +9,10 @@ const port = 3001;
 const runLighthouse = async () => {
   const server = await createServer(port);
   try {
-    const chrome = await launchChrome({ chromeFlags: [] });
+    const chrome = await launchChrome({
+      // https://github.com/GoogleChrome/chrome-launcher/issues/6
+      chromeFlags: ['--headless', '--disable-gpu', '--no-sandbox'],
+    });
     try {
       return await lighthouse(
         `http://localhost:${port}/`,
