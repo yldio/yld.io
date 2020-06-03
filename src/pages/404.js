5b2755eb8d42c635aeedb37cb7aa378f78d412c6
Tiago Gomes <t.dfgomes@icloud.com>
2020-06-03 11:39:41 +0100

    re-use 404 page components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,76 +1,28 @@
 import React from 'react';
-import { Grid, Row, Col } from '../components/grid';
-import breakpoint from 'styled-components-breakpoint';
-import { Link, StaticQuery, graphql } from 'gatsby';
+import { Grid } from '../components/grid';
+import { StaticQuery, graphql } from 'gatsby';
 import Helmet from 'react-helmet';
-import styled from 'styled-components';
-import remcalc from 'remcalc';
 import ReactMarkdown from 'react-markdown';
 
-import BlueBackground from '../components/Common/BlueBackground';
-import { SectionTitle, BodyPrimary } from '../components/Typography';
 import Layout from '../components/layout';
+
+import {
+  StyledBlueBackground,
+  StyledRow,
+  TextCol,
+  IllustrationCol,
+  TitleHeadline,
+  CopyText,
+  LinksTitle,
+  LinkParagraph,
+  NotFoundPageLink,
+} from '../components/404';
 
 import { LogoStyleContext } from '../context/PageContext';
 
 import illustration from '../images/404-illustration.svg';
 
 import { colors } from '../utils/theme';
-
-// Layout
-
-const StyledBlueBackground = styled(BlueBackground)`
-  margin-top: -${remcalc(36)};
-`;
-
-const StyledRow = styled(Row)`
-  align-items: center;
-`;
-
-const TextCol = styled(Col).attrs({
-  width: [1, 1, 1, 1, 0.5, 0.5],
-})`
-  padding-top: ${({ theme }) => theme.space[4]};
-  ${breakpoint('smallTablet')`
-    padding-bottom: ${({ theme }) => theme.space[4]};
-  `}
-  ${breakpoint('tablet')`
-    padding-top: ${({ theme }) => theme.space[7]};
-    padding-bottom: ${({ theme }) => theme.space[8]};
-  `}
-`;
-
-const IllustrationCol = styled(Col).attrs({
-  width: [1, 1, 1, 1, 0.5, 0.5],
-})`
-  ${breakpoint('smallTablet', 'tablet')`
-    padding-top: ${({ theme }) => theme.space[4]};
-    padding-bottom: ${({ theme }) => theme.space[4]};
-  `}
-`;
-
-// Paragraphs / Texts
-
-const TitleHeadline = props => (
-  <SectionTitle {...props} as="h1" reverse="true" />
-);
-
-const CopyText = props => <BodyPrimary {...props} reverse="true" />;
-
-const LinksTitle = styled(BodyPrimary).attrs({
-  reverse: 'true',
-  bold: 'true',
-})`
-  padding-top: ${({ theme }) => theme.space[3]};
-`;
-
-const LinkParagraph = props => (
-  <BodyPrimary {...props} reverse="true" noPaddingBottom="true" />
-);
-
-const NotFoundPageLink = styled(Link)`
-  text-decoration: underline;
-`;
 
 // Page
 
