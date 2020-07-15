386ad1c2f2004d0575931a01c109e4fc27bc68eb
Fábio Moreira <fabiommmoreira@gmail.com>
2020-07-15 15:48:10 +0100

    Join us casestudies match the looks of Our Work

--- src/components/JoinUs/Work.js
+++ src/components/JoinUs/Work.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { ColumnLayout } from '../grid';
-import { CaseStudy } from '../Common/CaseStudy';
+import CaseStudies from '../Common/case-studies/CaseStudies';
 import { Section, Separator, TitleAndBody } from './elements';
 import TitleAndList from '../Common/TitleAndList';
 import breakpoint from 'styled-components-breakpoint';
-
-import { getColorLuminance } from '../../utils/color';
 
 const MAX_CASE_STUDIES = 3;
 
@@ -34,24 +31,9 @@ const Work = ({ data: { title, subtitle, list, text, someWork } }) => (
     <TitleAndList title={title} list={list} />
     <Separator />
     <TitleAndBody title={subtitle} body={text} />
-    <ColumnLayout
-      cols={3}
-      items={someWork.slice(0, MAX_CASE_STUDIES)}
-      compensated
-    >
-      {({ Col, item: cs }) => (
-        <Col block={false}>
-          <CaseStudyWrapper>
-            <CaseStudy
-              bg={`#${cs.posterColor}`}
-              to={`/case-study/${cs.slug}`}
-              lightText={getColorLuminance(cs.posterColor) < 127.5}
-              title={cs.title}
-            />
-          </CaseStudyWrapper>
-        </Col>
-      )}
-    </ColumnLayout>
+    <CaseStudyWrapper>
+      <CaseStudies caseStudies={someWork.slice(0, MAX_CASE_STUDIES)} />
+    </CaseStudyWrapper>
     <SpaceFiller />
   </Section>
 );
