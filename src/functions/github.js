107a0baed5ef609b1d84b461c0d5a68fe8638328
Fábio Moreira <fabiommmoreira@gmail.com>
2020-08-04 15:50:34 +0100

    Consume members log from secret

--- src/functions/github.js
+++ src/functions/github.js
@@ -14,15 +14,7 @@ const Meta = require('./oss/meta');
 const Repos = require('./oss/repos');
 
 const org = 'yldio';
-
-const membersLog = `
-Sérgio Ramos,Joined YLD,Mar 2015,sergioramos
-Filipe Pinheiro,Joined YLD,Sep 2015,fampinheiro
-Fábio Moreira,Joined YLD,Apr 2018,fabiommmoreira
-Fábio Antunes (2),Joined YLD,Feb 2017,FabioAntunes
-Fábio Antunes (2),Left YLD,May 2018,FabioAntunes
-Fábio Antunes (1),Joined YLD,Mar 2019,FabioAntunes
-`;
+const membersLog = process.env.MEMBERS_LOG;
 
 exports.handler = async evt =>
   Auth(evt, async () => {
