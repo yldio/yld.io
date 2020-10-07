1bc5e407ce37ad9b8d09da09c91cdc5f0d05e6e5
Ricardo Barros <ricardofbarros@hotmail.com>
2020-10-07 11:56:52 +0100

    fix: outboundlink import

--- src/components/Common/ExternalAnchor.js
+++ src/components/Common/ExternalAnchor.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { OutboundLink } from 'gatsby-plugin-google-analytics';
+import { OutboundLink } from 'gatsby-plugin-google-gtag';
 
 const ExternalAnchor = ({ children, openInNewWindow = true, ...props }) => {
   return (
