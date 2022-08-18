97e69a036c4cd1dcc9eab5fff7cf00b0707fcb1a
Sérgio Ramos <mail@sergioramos.me>
2022-08-18 11:10:50 +0100

    feat: remove subsidiaries section

--- src/components/AboutUs/Partners.js
+++ src/components/AboutUs/Partners.js
@@ -3,20 +3,23 @@ import { Padding } from 'styled-components-spacing';
 
 import { Grid } from '../grid';
 import { SectionTitle } from '../Typography';
+import GreyBackground from '../Common/GreyBackground';
 import LogoGrid from '../Common/LogoGrid';
 
 const Partners = ({ title, partners }) => (
-  <Grid>
-    <Padding
-      top={{ smallPhone: 3, tablet: 4 }}
-      bottom={{ smallPhone: 3.5, tablet: 5 }}
-    >
-      <Padding bottom={{ smallPhone: 3, tablet: 4 }}>
-        <SectionTitle>{title}</SectionTitle>
+  <GreyBackground>
+    <Grid>
+      <Padding
+        top={{ smallPhone: 3, tablet: 4 }}
+        bottom={{ smallPhone: 3.5, tablet: 5 }}
+      >
+        <Padding bottom={{ smallPhone: 3, tablet: 4 }}>
+          <SectionTitle>{title}</SectionTitle>
+        </Padding>
+        <LogoGrid companies={partners} />
       </Padding>
-      <LogoGrid companies={partners} />
-    </Padding>
-  </Grid>
+    </Grid>
+  </GreyBackground>
 );
 
 export default Partners;
